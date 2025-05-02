e80037fdcfcf84c9ea714988069e6d0864d40c4a
Alvaro-Camilo-Vargas-Gonzalez <alvaro.camilo@itssglobal.com>
2025-05-02 15:46:04 -0600

    Add files via upload

---
+++ AaAcAcctGroupCondnFields.java
@@ -0,0 +1,224 @@
+package com;
+
+import com.temenos.t24.AA_AC_ACCT_GROUP_CONDN_FIELDS_cl;
+import com.temenos.t24.component_AA_ClassicProducts_22_cl;
+import com.temenos.t24.component_EB_SystemTables_19_cl;
+import com.temenos.t24.component_IC_OtherInterest_20_cl;
+import com.temenos.t24.component_ST_Config_14_cl;
+import com.temenos.tafj.common.jSession;
+import com.temenos.tafj.common.jVar;
+
+public class AaAcAcctGroupCondnFields extends AA_AC_ACCT_GROUP_CONDN_FIELDS_cl {
+
+    @Override
+    protected void CB(int arg0) {
+        // TODO Auto-generated method stub
+        super.CB(arg0);
+    }
+
+    @Override
+    public void CLEAR() {
+        // TODO Auto-generated method stub
+        super.CLEAR();
+    }
+
+    @Override
+    protected void GOSUB(int arg0, boolean arg1) {
+        // TODO Auto-generated method stub
+        super.GOSUB(arg0, arg1);
+    }
+
+    @Override
+    protected void GOSUB(int arg0) {
+        // TODO Auto-generated method stub
+        super.GOSUB(arg0);
+    }
+
+    @Override
+    public void create() {
+        // TODO Auto-generated method stub
+        super.create();
+    }
+
+    @Override
+    public component_AA_ClassicProducts_22_cl getAA_ClassicProducts() {
+        // TODO Auto-generated method stub
+        return super.getAA_ClassicProducts();
+    }
+
+    @Override
+    public String getBASICName() {
+        // TODO Auto-generated method stub
+        return super.getBASICName();
+    }
+
+    @Override
+    public String getCompileInfo() {
+        // TODO Auto-generated method stub
+        return super.getCompileInfo();
+    }
+
+    @Override
+    public String[] getComponentList() {
+        // TODO Auto-generated method stub
+        return super.getComponentList();
+    }
+
+    @Override
+    public jVar getDataStructureFields(java.lang.Object arg0) {
+        // TODO Auto-generated method stub
+        return super.getDataStructureFields(arg0);
+    }
+
+    @Override
+    public jVar getDataStructureNames() {
+        // TODO Auto-generated method stub
+        return super.getDataStructureNames();
+    }
+
+    @Override
+    public component_EB_SystemTables_19_cl getEB_SystemTables() {
+        // TODO Auto-generated method stub
+        return super.getEB_SystemTables();
+    }
+
+    @Override
+    public component_IC_OtherInterest_20_cl getIC_OtherInterest() {
+        // TODO Auto-generated method stub
+        return super.getIC_OtherInterest();
+    }
+
+    @Override
+    public String getImportBasic() {
+        // TODO Auto-generated method stub
+        return super.getImportBasic();
+    }
+
+    @Override
+    public int getNbLines() {
+        // TODO Auto-generated method stub
+        return super.getNbLines();
+    }
+
+    @Override
+    public String getPackageBasic() {
+        // TODO Auto-generated method stub
+        return super.getPackageBasic();
+    }
+
+    @Override
+    public String[] getParamList() {
+        // TODO Auto-generated method stub
+        return super.getParamList();
+    }
+
+    @Override
+    public jVar[] getParams() {
+        // TODO Auto-generated method stub
+        return super.getParams();
+    }
+
+    @Override
+    public String getPathFileNameBasic() {
+        // TODO Auto-generated method stub
+        return super.getPathFileNameBasic();
+    }
+
+    @Override
+    public String getReplacementInfo() {
+        // TODO Auto-generated method stub
+        return super.getReplacementInfo();
+    }
+
+    @Override
+    public component_ST_Config_14_cl getST_Config() {
+        // TODO Auto-generated method stub
+        return super.getST_Config();
+    }
+
+    @Override
+    public String[] getVarList() {
+        // TODO Auto-generated method stub
+        return super.getVarList();
+    }
+
+    @Override
+    public String getVarValue(String arg0) {
+        // TODO Auto-generated method stub
+        return super.getVarValue(arg0);
+    }
+
+    @Override
+    public String getVersion() {
+        // TODO Auto-generated method stub
+        return super.getVersion();
+    }
+
+    @Override
+    public void init(jSession arg0) {
+        // TODO Auto-generated method stub
+        super.init(arg0);
+    }
+
+    @Override
+    public jVar invoke(java.lang.Object arg0, java.lang.Object arg1, java.lang.Object arg2, java.lang.Object arg3,
+            java.lang.Object arg4, java.lang.Object arg5, java.lang.Object arg6, java.lang.Object arg7) {
+        // TODO Auto-generated method stub
+        return super.invoke(arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+    }
+
+    @Override
+    public jVar invoke(java.lang.Object... arg0) {
+        // TODO Auto-generated method stub
+        return super.invoke(arg0);
+    }
+
+    @Override
+    public void keepMoving() {
+        // TODO Auto-generated method stub
+        super.keepMoving();
+    }
+
+    @Override
+    protected int main() {
+        // TODO Auto-generated method stub
+        return super.main();
+    }
+
+    @Override
+    public void move() {
+        // TODO Auto-generated method stub
+        super.move();
+    }
+
+    @Override
+    public void release() {
+        // TODO Auto-generated method stub
+        super.release();
+    }
+
+    @Override
+    public void reset() {
+        // TODO Auto-generated method stub
+        super.reset();
+    }
+
+    @Override
+    public String setVarValue(String arg0, String arg1) {
+        // TODO Auto-generated method stub
+        return super.setVarValue(arg0, arg1);
+    }
+
+    @Override
+    public void stack(AA_AC_ACCT_GROUP_CONDN_FIELDS_cl arg0) {
+        // TODO Auto-generated method stub
+        super.stack(arg0);
+    }
+
+    @Override
+    public void stopMoving() {
+        // TODO Auto-generated method stub
+        super.stopMoving();
+    }
+    
+}
