46243780b1fd48d83957e9bbf7a6b449587ccafe
Alvaro-Camilo-Vargas-Gonzalez <alvaro.camilo@itssglobal.com>
2025-06-11 11:57:47 -0600

    Add files via upload

---
+++ src/com/AaAccountClosureNotice.java
@@ -0,0 +1,270 @@
+package com;
+
+import com.temenos.t24.AA_ACCOUNT_CLOSURE_NOTICE_cl;
+import com.temenos.t24.component_AA_NoticeWithdrawal_23_cl;
+import com.temenos.t24.component_AA_PaymentSchedule_22_cl;
+import com.temenos.tafj.common.jSession;
+import com.temenos.tafj.common.jVar;
+
+public class AaAccountClosureNotice extends AA_ACCOUNT_CLOSURE_NOTICE_cl {
+
+    @Override
+    protected void CB(int arg0) {
+        
+        super.CB(arg0);
+    }
+
+    @Override
+    public void CLEAR() {
+        
+        super.CLEAR();
+    }
+
+    @Override
+    protected void GOSUB(int arg0, boolean arg1) {
+        
+        super.GOSUB(arg0, arg1);
+    }
+
+    @Override
+    protected void GOSUB(int arg0) {
+        
+        super.GOSUB(arg0);
+    }
+
+    @Override
+    public void create() {
+        
+        super.create();
+    }
+
+    @Override
+    public component_AA_NoticeWithdrawal_23_cl getAA_NoticeWithdrawal() {
+        
+        return super.getAA_NoticeWithdrawal();
+    }
+
+    @Override
+    public component_AA_PaymentSchedule_22_cl getAA_PaymentSchedule() {
+        
+        return super.getAA_PaymentSchedule();
+    }
+
+    @Override
+    public String getBASICName() {
+        
+        return super.getBASICName();
+    }
+
+    @Override
+    public String getCompileInfo() {
+        
+        return super.getCompileInfo();
+    }
+
+    @Override
+    public String[] getComponentList() {
+        
+        return super.getComponentList();
+    }
+
+    @Override
+    public jVar getDataStructureFields(java.lang.Object arg0) {
+        
+        return super.getDataStructureFields(arg0);
+    }
+
+    @Override
+    public jVar getDataStructureNames() {
+        
+        return super.getDataStructureNames();
+    }
+
+    @Override
+    public String getImportBasic() {
+        
+        return super.getImportBasic();
+    }
+
+    @Override
+    public int getNbLines() {
+        
+        return super.getNbLines();
+    }
+
+    @Override
+    public String getPackageBasic() {
+        
+        return super.getPackageBasic();
+    }
+
+    @Override
+    public String[] getParamList() {
+        
+        return super.getParamList();
+    }
+
+    @Override
+    public jVar[] getParams() {
+        
+        return super.getParams();
+    }
+
+    @Override
+    public String getPathFileNameBasic() {
+        
+        return super.getPathFileNameBasic();
+    }
+
+    @Override
+    public String getReplacementInfo() {
+        
+        return super.getReplacementInfo();
+    }
+
+    @Override
+    public String[] getVarList() {
+        
+        return super.getVarList();
+    }
+
+    @Override
+    public String getVarValue(String arg0) {
+        
+        return super.getVarValue(arg0);
+    }
+
+    @Override
+    public String getVersion() {
+        
+        return super.getVersion();
+    }
+
+    @Override
+    public void init(jSession arg0) {
+        
+        super.init(arg0);
+    }
+
+    @Override
+    public jVar invoke(java.lang.Object arg0, java.lang.Object arg1, java.lang.Object arg2, java.lang.Object arg3,
+            java.lang.Object arg4, java.lang.Object arg5, java.lang.Object arg6) {
+        
+        return super.invoke(arg0, arg1, arg2, arg3, arg4, arg5, arg6);
+    }
+
+    @Override
+    public jVar invoke(java.lang.Object... arg0) {
+        
+        return super.invoke(arg0);
+    }
+
+    @Override
+    public void keepMoving() {
+        
+        super.keepMoving();
+    }
+
+    @Override
+    protected int lbl_GenBillId() {
+        
+        return super.lbl_GenBillId();
+    }
+
+    @Override
+    protected int lbl_GetBill() {
+        
+        return super.lbl_GetBill();
+    }
+
+    @Override
+    protected int lbl_GetBillDetails() {
+        
+        return super.lbl_GetBillDetails();
+    }
+
+    @Override
+    protected int lbl_Initialise() {
+        
+        return super.lbl_Initialise();
+    }
+
+    @Override
+    protected int lbl_Process() {
+        
+        return super.lbl_Process();
+    }
+
+    @Override
+    protected int lbl_ProcessInputAction() {
+        
+        return super.lbl_ProcessInputAction();
+    }
+
+    @Override
+    protected int lbl_ProcessReverseAction() {
+        
+        return super.lbl_ProcessReverseAction();
+    }
+
+    @Override
+    protected int lbl_UpdateBillDetails() {
+        
+        return super.lbl_UpdateBillDetails();
+    }
+
+    @Override
+    protected int lbl_UpdateBillStatus() {
+        
+        return super.lbl_UpdateBillStatus();
+    }
+
+    @Override
+    protected int lbl_WriteBillDetails() {
+        
+        return super.lbl_WriteBillDetails();
+    }
+
+    @Override
+    protected int main() {
+        
+        return super.main();
+    }
+
+    @Override
+    public void move() {
+        
+        super.move();
+    }
+
+    @Override
+    public void release() {
+        
+        super.release();
+    }
+
+    @Override
+    public void reset() {
+        
+        super.reset();
+    }
+
+    @Override
+    public String setVarValue(String arg0, String arg1) {
+        
+        return super.setVarValue(arg0, arg1);
+    }
+
+    @Override
+    public void stack(AA_ACCOUNT_CLOSURE_NOTICE_cl arg0) {
+        
+        super.stack(arg0);
+    }
+
+    @Override
+    public void stopMoving() {
+        
+        super.stopMoving();
+    }
+
+}
