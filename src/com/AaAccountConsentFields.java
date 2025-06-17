294e93768c32e6bc5ca8d7ce3e27d226dd301cbe
Alvaro-Camilo-Vargas-Gonzalez <alvaro.camilo@itssglobal.com>
2025-06-17 16:23:37 -0600

    Add files via upload

---
+++ src/com/AaAccountConsentFields.java
@@ -0,0 +1,223 @@
+package com;
+
+import com.temenos.t24.AA_ACCOUNT_CONSENT_FIELDS_cl;
+import com.temenos.t24.component_CK_Consent_15_cl;
+import com.temenos.t24.component_EB_Template_16_cl;
+import com.temenos.t24.component_PZ_Consent_15_cl;
+import com.temenos.tafj.common.jSession;
+import com.temenos.tafj.common.jVar;
+
+public class AaAccountConsentFields extends AA_ACCOUNT_CONSENT_FIELDS_cl {
+
+    @Override
+    protected void CB(int arg0) {
+
+        super.CB(arg0);
+    }
+
+    @Override
+    public void CLEAR() {
+
+        super.CLEAR();
+    }
+
+    @Override
+    protected void GOSUB(int arg0, boolean arg1) {
+
+        super.GOSUB(arg0, arg1);
+    }
+
+    @Override
+    protected void GOSUB(int arg0) {
+
+        super.GOSUB(arg0);
+    }
+
+    @Override
+    public void create() {
+
+        super.create();
+    }
+
+    @Override
+    public String getBASICName() {
+
+        return super.getBASICName();
+    }
+
+    @Override
+    public component_CK_Consent_15_cl getCK_Consent() {
+
+        return super.getCK_Consent();
+    }
+
+    @Override
+    public String getCompileInfo() {
+
+        return super.getCompileInfo();
+    }
+
+    @Override
+    public String[] getComponentList() {
+
+        return super.getComponentList();
+    }
+
+    @Override
+    public jVar getDataStructureFields(java.lang.Object arg0) {
+
+        return super.getDataStructureFields(arg0);
+    }
+
+    @Override
+    public jVar getDataStructureNames() {
+
+        return super.getDataStructureNames();
+    }
+
+    @Override
+    public component_EB_Template_16_cl getEB_Template() {
+
+        return super.getEB_Template();
+    }
+
+    @Override
+    public String getImportBasic() {
+
+        return super.getImportBasic();
+    }
+
+    @Override
+    public int getNbLines() {
+
+        return super.getNbLines();
+    }
+
+    @Override
+    public component_PZ_Consent_15_cl getPZ_Consent() {
+
+        return super.getPZ_Consent();
+    }
+
+    @Override
+    public String getPackageBasic() {
+
+        return super.getPackageBasic();
+    }
+
+    @Override
+    public String[] getParamList() {
+
+        return super.getParamList();
+    }
+
+    @Override
+    public jVar[] getParams() {
+
+        return super.getParams();
+    }
+
+    @Override
+    public String getPathFileNameBasic() {
+
+        return super.getPathFileNameBasic();
+    }
+
+    @Override
+    public String getReplacementInfo() {
+
+        return super.getReplacementInfo();
+    }
+
+    @Override
+    public String[] getVarList() {
+
+        return super.getVarList();
+    }
+
+    @Override
+    public String getVarValue(String arg0) {
+
+        return super.getVarValue(arg0);
+    }
+
+    @Override
+    public String getVersion() {
+
+        return super.getVersion();
+    }
+
+    @Override
+    public void init(jSession arg0) {
+
+        super.init(arg0);
+    }
+
+    @Override
+    public jVar invoke(java.lang.Object arg0, java.lang.Object arg1, java.lang.Object arg2, java.lang.Object arg3,
+            java.lang.Object arg4, java.lang.Object arg5, java.lang.Object arg6, java.lang.Object arg7) {
+
+        return super.invoke(arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+    }
+
+    @Override
+    public jVar invoke(java.lang.Object... arg0) {
+
+        return super.invoke(arg0);
+    }
+
+    @Override
+    public void keepMoving() {
+
+        super.keepMoving();
+    }
+
+    @Override
+    protected int lbl_DEFINE_FIELDS() {
+
+        return super.lbl_DEFINE_FIELDS();
+    }
+
+    @Override
+    protected int main() {
+
+        return super.main();
+    }
+
+    @Override
+    public void move() {
+
+        super.move();
+    }
+
+    @Override
+    public void release() {
+
+        super.release();
+    }
+
+    @Override
+    public void reset() {
+
+        super.reset();
+    }
+
+    @Override
+    public String setVarValue(String arg0, String arg1) {
+
+        return super.setVarValue(arg0, arg1);
+    }
+
+    @Override
+    public void stack(AA_ACCOUNT_CONSENT_FIELDS_cl arg0) {
+
+        super.stack(arg0);
+    }
+
+    @Override
+    public void stopMoving() {
+
+        super.stopMoving();
+    }
+
+}
