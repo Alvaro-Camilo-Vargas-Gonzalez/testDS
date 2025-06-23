1debe50f3ebeea04c7ff8bfb3095e1d18b0cc7a7
Alvaro-Camilo-Vargas-Gonzalez <alvaro.camilo@itssglobal.com>
2025-06-23 12:20:13 -0600

    Add files via upload

---
+++ src/com/AaAccountConsentValidate.java
@@ -0,0 +1,352 @@
+package com;
+
+import com.temenos.t24.AA_ACCOUNT_CONSENT_VALIDATE_cl;
+import com.temenos.t24.component_AA_Framework_17_cl;
+import com.temenos.t24.component_AC_AccountOpening_21_cl;
+import com.temenos.t24.component_AF_Framework_17_cl;
+import com.temenos.t24.component_EB_DataAccess_17_cl;
+import com.temenos.t24.component_EB_ErrorProcessing_22_cl;
+import com.temenos.t24.component_EB_Interface_17_cl;
+import com.temenos.t24.component_EB_SystemTables_19_cl;
+import com.temenos.t24.component_EB_Template_16_cl;
+import com.temenos.t24.component_EB_Utility_15_cl;
+import com.temenos.t24.component_PZ_Config_14_cl;
+import com.temenos.t24.component_PZ_Consent_15_cl;
+import com.temenos.t24.component_RT_OpenBanking_18_cl;
+import com.temenos.t24.component_ST_CompanyCreation_22_cl;
+import com.temenos.tafj.common.jSession;
+import com.temenos.tafj.common.jVar;
+
+public class AaAccountConsentValidate extends AA_ACCOUNT_CONSENT_VALIDATE_cl {
+
+    @Override
+    protected void CB(int arg0) {
+        
+        super.CB(arg0);
+    }
+
+    @Override
+    public void CLEAR() {
+        
+        super.CLEAR();
+    }
+
+    @Override
+    protected void GOSUB(int arg0, boolean arg1) {
+        
+        super.GOSUB(arg0, arg1);
+    }
+
+    @Override
+    protected void GOSUB(int arg0) {
+        
+        super.GOSUB(arg0);
+    }
+
+    @Override
+    public void create() {
+        
+        super.create();
+    }
+
+    @Override
+    public component_AA_Framework_17_cl getAA_Framework() {
+        
+        return super.getAA_Framework();
+    }
+
+    @Override
+    public component_AC_AccountOpening_21_cl getAC_AccountOpening() {
+        
+        return super.getAC_AccountOpening();
+    }
+
+    @Override
+    public component_AF_Framework_17_cl getAF_Framework() {
+        
+        return super.getAF_Framework();
+    }
+
+    @Override
+    public String getBASICName() {
+        
+        return super.getBASICName();
+    }
+
+    @Override
+    public String getCompileInfo() {
+        
+        return super.getCompileInfo();
+    }
+
+    @Override
+    public String[] getComponentList() {
+        
+        return super.getComponentList();
+    }
+
+    @Override
+    public jVar getDataStructureFields(java.lang.Object arg0) {
+        
+        return super.getDataStructureFields(arg0);
+    }
+
+    @Override
+    public jVar getDataStructureNames() {
+        
+        return super.getDataStructureNames();
+    }
+
+    @Override
+    public component_EB_DataAccess_17_cl getEB_DataAccess() {
+        
+        return super.getEB_DataAccess();
+    }
+
+    @Override
+    public component_EB_ErrorProcessing_22_cl getEB_ErrorProcessing() {
+        
+        return super.getEB_ErrorProcessing();
+    }
+
+    @Override
+    public component_EB_Interface_17_cl getEB_Interface() {
+        
+        return super.getEB_Interface();
+    }
+
+    @Override
+    public component_EB_SystemTables_19_cl getEB_SystemTables() {
+        
+        return super.getEB_SystemTables();
+    }
+
+    @Override
+    public component_EB_Template_16_cl getEB_Template() {
+        
+        return super.getEB_Template();
+    }
+
+    @Override
+    public component_EB_Utility_15_cl getEB_Utility() {
+        
+        return super.getEB_Utility();
+    }
+
+    @Override
+    public String getImportBasic() {
+        
+        return super.getImportBasic();
+    }
+
+    @Override
+    public int getNbLines() {
+        
+        return super.getNbLines();
+    }
+
+    @Override
+    public component_PZ_Config_14_cl getPZ_Config() {
+        
+        return super.getPZ_Config();
+    }
+
+    @Override
+    public component_PZ_Consent_15_cl getPZ_Consent() {
+        
+        return super.getPZ_Consent();
+    }
+
+    @Override
+    public String getPackageBasic() {
+        
+        return super.getPackageBasic();
+    }
+
+    @Override
+    public String[] getParamList() {
+        
+        return super.getParamList();
+    }
+
+    @Override
+    public jVar[] getParams() {
+        
+        return super.getParams();
+    }
+
+    @Override
+    public String getPathFileNameBasic() {
+        
+        return super.getPathFileNameBasic();
+    }
+
+    @Override
+    public component_RT_OpenBanking_18_cl getRT_OpenBanking() {
+        
+        return super.getRT_OpenBanking();
+    }
+
+    @Override
+    public String getReplacementInfo() {
+        
+        return super.getReplacementInfo();
+    }
+
+    @Override
+    public component_ST_CompanyCreation_22_cl getST_CompanyCreation() {
+        
+        return super.getST_CompanyCreation();
+    }
+
+    @Override
+    public String[] getVarList() {
+
+        return super.getVarList();
+    }
+
+    @Override
+    public String getVarValue(String arg0) {
+
+        return super.getVarValue(arg0);
+    }
+
+    @Override
+    public String getVersion() {
+
+        return super.getVersion();
+    }
+
+    @Override
+    public void init(jSession arg0) {
+
+        super.init(arg0);
+    }
+
+    @Override
+    public jVar invoke() {
+
+        return super.invoke();
+    }
+
+    @Override
+    public jVar invoke(java.lang.Object... arg0) {
+
+        return super.invoke(arg0);
+    }
+
+    @Override
+    public void keepMoving() {
+
+        super.keepMoving();
+    }
+
+    @Override
+    protected int lbl_ASSIGN_STATUS() {
+
+        return super.lbl_ASSIGN_STATUS();
+    }
+
+    @Override
+    protected int lbl_ArrangementCrossval() {
+
+        return super.lbl_ArrangementCrossval();
+    }
+
+    @Override
+    protected int lbl_DEFAULT_ACCOUNT_IDS() {
+
+        return super.lbl_DEFAULT_ACCOUNT_IDS();
+    }
+
+    @Override
+    protected int lbl_DEFAULT_EXPIRY_DATE() {
+
+        return super.lbl_DEFAULT_EXPIRY_DATE();
+    }
+
+    @Override
+    protected int lbl_DEFAULT_TPP_NAME() {
+
+        return super.lbl_DEFAULT_TPP_NAME();
+    }
+
+    @Override
+    protected int lbl_DesignerCrossval() {
+
+        return super.lbl_DesignerCrossval();
+    }
+
+    @Override
+    protected int lbl_FldNotAllowed() {
+
+        return super.lbl_FldNotAllowed();
+    }
+
+    @Override
+    protected int lbl_INITIALISE() {
+
+        return super.lbl_INITIALISE();
+    }
+
+    @Override
+    protected int lbl_VALIDATE_ACCOUNT() {
+
+        return super.lbl_VALIDATE_ACCOUNT();
+    }
+
+    @Override
+    protected int lbl_VALIDATE_TPP_REF() {
+
+        return super.lbl_VALIDATE_TPP_REF();
+    }
+
+    @Override
+    protected int lbl_checkSignupService() {
+
+        return super.lbl_checkSignupService();
+    }
+
+    @Override
+    protected int main() {
+
+        return super.main();
+    }
+
+    @Override
+    public void move() {
+
+        super.move();
+    }
+
+    @Override
+    public void release() {
+
+        super.release();
+    }
+
+    @Override
+    public void reset() {
+
+        super.reset();
+    }
+
+    @Override
+    public String setVarValue(String arg0, String arg1) {
+
+        return super.setVarValue(arg0, arg1);
+    }
+
+    @Override
+    public void stack(AA_ACCOUNT_CONSENT_VALIDATE_cl arg0) {
+
+        super.stack(arg0);
+    }
+
+    @Override
+    public void stopMoving() {
+
+        super.stopMoving();
+    }
+
+}
