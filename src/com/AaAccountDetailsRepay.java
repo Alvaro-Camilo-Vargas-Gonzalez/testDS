65a5fb830ca4f3247e40d571b45bb43146bc857d
Alvaro-Camilo-Vargas-Gonzalez <alvaro.camilo@itssglobal.com>
2025-07-14 13:15:50 -0600

    Add files via upload

---
+++ src/com/AaAccountDetailsRepay.java
@@ -0,0 +1,309 @@
+package com;
+
+import com.temenos.t24.AA_ACCOUNT_DETAILS_REPAY_cl;
+import com.temenos.t24.component_AA_Framework_17_cl;
+import com.temenos.t24.component_AA_Overdue_15_cl;
+import com.temenos.t24.component_AA_PaymentSchedule_22_cl;
+import com.temenos.t24.component_AA_ProductFramework_23_cl;
+import com.temenos.t24.component_AF_Framework_17_cl;
+import com.temenos.tafj.common.jSession;
+import com.temenos.tafj.common.jVar;
+
+public class AaAccountDetailsRepay extends AA_ACCOUNT_DETAILS_REPAY_cl {
+
+    @Override
+    protected void CB(int arg0) {
+        
+        super.CB(arg0);
+    }
+
+    @Override
+    public void CLEAR() {
+        
+        super.CLEAR();
+    }
+
+    @Override
+    protected void GOSUB(int arg0, boolean arg1) {
+        
+        super.GOSUB(arg0, arg1);
+    }
+
+    @Override
+    protected void GOSUB(int arg0) {
+        
+        super.GOSUB(arg0);
+    }
+
+    @Override
+    public void create() {
+        
+        super.create();
+    }
+
+    @Override
+    public component_AA_Framework_17_cl getAA_Framework() {
+        
+        return super.getAA_Framework();
+    }
+
+    @Override
+    public component_AA_Overdue_15_cl getAA_Overdue() {
+        
+        return super.getAA_Overdue();
+    }
+
+    @Override
+    public component_AA_PaymentSchedule_22_cl getAA_PaymentSchedule() {
+        
+        return super.getAA_PaymentSchedule();
+    }
+
+    @Override
+    public component_AA_ProductFramework_23_cl getAA_ProductFramework() {
+        
+        return super.getAA_ProductFramework();
+    }
+
+    @Override
+    public component_AF_Framework_17_cl getAF_Framework() {
+        
+        return super.getAF_Framework();
+    }
+
+    @Override
+    public String getBASICName() {
+        
+        return super.getBASICName();
+    }
+
+    @Override
+    public String getCompileInfo() {
+        
+        return super.getCompileInfo();
+    }
+
+    @Override
+    public String[] getComponentList() {
+        
+        return super.getComponentList();
+    }
+
+    @Override
+    public jVar getDataStructureFields(java.lang.Object arg0) {
+        
+        return super.getDataStructureFields(arg0);
+    }
+
+    @Override
+    public jVar getDataStructureNames() {
+        
+        return super.getDataStructureNames();
+    }
+
+    @Override
+    public String getImportBasic() {
+        
+        return super.getImportBasic();
+    }
+
+    @Override
+    public int getNbLines() {
+        
+        return super.getNbLines();
+    }
+
+    @Override
+    public String getPackageBasic() {
+        
+        return super.getPackageBasic();
+    }
+
+    @Override
+    public String[] getParamList() {
+        
+        return super.getParamList();
+    }
+
+    @Override
+    public jVar[] getParams() {
+        
+        return super.getParams();
+    }
+
+    @Override
+    public String getPathFileNameBasic() {
+        
+        return super.getPathFileNameBasic();
+    }
+
+    @Override
+    public String getReplacementInfo() {
+        
+        return super.getReplacementInfo();
+    }
+
+    @Override
+    public String[] getVarList() {
+        
+        return super.getVarList();
+    }
+
+    @Override
+    public String getVarValue(String arg0) {
+        
+        return super.getVarValue(arg0);
+    }
+
+    @Override
+    public String getVersion() {
+        
+        return super.getVersion();
+    }
+
+    @Override
+    public void init(jSession arg0) {
+        
+        super.init(arg0);
+    }
+
+    @Override
+    public jVar invoke(java.lang.Object arg0, java.lang.Object arg1, java.lang.Object arg2, java.lang.Object arg3,
+            java.lang.Object arg4, java.lang.Object arg5, java.lang.Object arg6, java.lang.Object arg7) {
+        
+        return super.invoke(arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7);
+    }
+
+    @Override
+    public jVar invoke(java.lang.Object... arg0) {
+        
+        return super.invoke(arg0);
+    }
+
+    @Override
+    public void keepMoving() {
+        
+        super.keepMoving();
+    }
+
+    @Override
+    protected int lbl_CHECK_REQD_INFO() {
+        
+        return super.lbl_CHECK_REQD_INFO();
+    }
+
+    @Override
+    protected int lbl_GET_ACCOUNT_DETAILS() {
+        
+        return super.lbl_GET_ACCOUNT_DETAILS();
+    }
+
+    @Override
+    protected int lbl_GET_BILL_DETAILS() {
+        
+        return super.lbl_GET_BILL_DETAILS();
+    }
+
+    @Override
+    protected int lbl_GET_CHASER_ACTIVITY_STATUS() {
+        
+        return super.lbl_GET_CHASER_ACTIVITY_STATUS();
+    }
+
+    @Override
+    protected int lbl_GET_REPAY_DETAILS() {
+        
+        return super.lbl_GET_REPAY_DETAILS();
+    }
+
+    @Override
+    protected int lbl_GET_STATS_PROCESSING_DETAILS() {
+        
+        return super.lbl_GET_STATS_PROCESSING_DETAILS();
+    }
+
+    @Override
+    protected int lbl_INITIALISE() {
+        
+        return super.lbl_INITIALISE();
+    }
+
+    @Override
+    protected int lbl_PROCESS_OVERDUE_STATS() {
+        
+        return super.lbl_PROCESS_OVERDUE_STATS();
+    }
+
+    @Override
+    protected int lbl_PROCESS_REPAY_DETAILS() {
+        
+        return super.lbl_PROCESS_REPAY_DETAILS();
+    }
+
+    @Override
+    protected int lbl_UPDATE_NEXT_AGE_CHASER_DATE() {
+        
+        return super.lbl_UPDATE_NEXT_AGE_CHASER_DATE();
+    }
+
+    @Override
+    protected int lbl_UPDATE_REPAY_BILLS() {
+        
+        return super.lbl_UPDATE_REPAY_BILLS();
+    }
+
+    @Override
+    protected int lbl_UPDATE_REPAY_DETAILS() {
+        
+        return super.lbl_UPDATE_REPAY_DETAILS();
+    }
+
+    @Override
+    protected int lbl_UPDATE_REVERSE_DETAILS() {
+        
+        return super.lbl_UPDATE_REVERSE_DETAILS();
+    }
+
+    @Override
+    protected int main() {
+        
+        return super.main();
+    }
+
+    @Override
+    public void move() {
+        
+        super.move();
+    }
+
+    @Override
+    public void release() {
+        
+        super.release();
+    }
+
+    @Override
+    public void reset() {
+        
+        super.reset();
+    }
+
+    @Override
+    public String setVarValue(String arg0, String arg1) {
+        
+        return super.setVarValue(arg0, arg1);
+    }
+
+    @Override
+    public void stack(AA_ACCOUNT_DETAILS_REPAY_cl arg0) {
+        
+        super.stack(arg0);
+    }
+
+    @Override
+    public void stopMoving() {
+        
+        super.stopMoving();
+    }
+
+}
