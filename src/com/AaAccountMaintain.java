0a022f4ef2caa95a0b8e6047ace23853b58b6cd4
Alvaro-Camilo-Vargas-Gonzalez <alvaro.camilo@itssglobal.com>
2025-08-05 13:38:47 -0600

    Add files via upload

---
+++ src/com/AaAccountMaintain.java
@@ -0,0 +1,683 @@
+package com;
+
+import com.temenos.t24.AA_ACCOUNT_MAINTAIN_cl;
+import com.temenos.t24.component_AA_Account_15_cl;
+import com.temenos.t24.component_AA_Accounting_18_cl;
+import com.temenos.t24.component_AA_BundleHierarchy_22_cl;
+import com.temenos.t24.component_AA_Closure_15_cl;
+import com.temenos.t24.component_AA_Customer_16_cl;
+import com.temenos.t24.component_AA_Framework_17_cl;
+import com.temenos.t24.component_AA_Limit_13_cl;
+import com.temenos.t24.component_AA_PaymentSchedule_22_cl;
+import com.temenos.t24.component_AA_Payoff_14_cl;
+import com.temenos.t24.component_AA_PricingGrid_18_cl;
+import com.temenos.t24.component_AA_ProductFramework_23_cl;
+import com.temenos.t24.component_AA_Reporting_17_cl;
+import com.temenos.t24.component_AB_Framework_17_cl;
+import com.temenos.t24.component_AC_API_9_cl;
+import com.temenos.t24.component_AC_AccountOpening_21_cl;
+import com.temenos.t24.component_AC_BalanceUpdates_21_cl;
+import com.temenos.t24.component_AC_HighVolume_17_cl;
+import com.temenos.t24.component_AF_Framework_17_cl;
+import com.temenos.t24.component_EB_API_9_cl;
+import com.temenos.t24.component_EB_ErrorProcessing_22_cl;
+import com.temenos.t24.component_EB_OverrideProcessing_25_cl;
+import com.temenos.t24.component_EB_Service_15_cl;
+import com.temenos.t24.component_EB_SystemTables_19_cl;
+import com.temenos.t24.component_IN_IbanAPI_12_cl;
+import com.temenos.t24.component_LI_Config_14_cl;
+import com.temenos.t24.component_ST_CompanyCreation_22_cl;
+import com.temenos.tafj.common.jSession;
+import com.temenos.tafj.common.jVar;
+
+public class AaAccountMaintain extends AA_ACCOUNT_MAINTAIN_cl {
+
+    @Override
+    protected void CB(int arg0) {
+        
+        super.CB(arg0);
+    }
+
+    @Override
+    public void CLEAR() {
+        
+        super.CLEAR();
+    }
+
+    @Override
+    protected void GOSUB(int arg0, boolean arg1) {
+        
+        super.GOSUB(arg0, arg1);
+    }
+
+    @Override
+    protected void GOSUB(int arg0) {
+        
+        super.GOSUB(arg0);
+    }
+
+    @Override
+    public void create() {
+        
+        super.create();
+    }
+
+    @Override
+    public component_AA_Account_15_cl getAA_Account() {
+        
+        return super.getAA_Account();
+    }
+
+    @Override
+    public component_AA_Accounting_18_cl getAA_Accounting() {
+        
+        return super.getAA_Accounting();
+    }
+
+    @Override
+    public component_AA_BundleHierarchy_22_cl getAA_BundleHierarchy() {
+        
+        return super.getAA_BundleHierarchy();
+    }
+
+    @Override
+    public component_AA_Closure_15_cl getAA_Closure() {
+        
+        return super.getAA_Closure();
+    }
+
+    @Override
+    public component_AA_Customer_16_cl getAA_Customer() {
+        
+        return super.getAA_Customer();
+    }
+
+    @Override
+    public component_AA_Framework_17_cl getAA_Framework() {
+        
+        return super.getAA_Framework();
+    }
+
+    @Override
+    public component_AA_Limit_13_cl getAA_Limit() {
+        
+        return super.getAA_Limit();
+    }
+
+    @Override
+    public component_AA_PaymentSchedule_22_cl getAA_PaymentSchedule() {
+        
+        return super.getAA_PaymentSchedule();
+    }
+
+    @Override
+    public component_AA_Payoff_14_cl getAA_Payoff() {
+        
+        return super.getAA_Payoff();
+    }
+
+    @Override
+    public component_AA_PricingGrid_18_cl getAA_PricingGrid() {
+        
+        return super.getAA_PricingGrid();
+    }
+
+    @Override
+    public component_AA_ProductFramework_23_cl getAA_ProductFramework() {
+        
+        return super.getAA_ProductFramework();
+    }
+
+    @Override
+    public component_AA_Reporting_17_cl getAA_Reporting() {
+        
+        return super.getAA_Reporting();
+    }
+
+    @Override
+    public component_AB_Framework_17_cl getAB_Framework() {
+        
+        return super.getAB_Framework();
+    }
+
+    @Override
+    public component_AC_API_9_cl getAC_API() {
+        
+        return super.getAC_API();
+    }
+
+    @Override
+    public component_AC_AccountOpening_21_cl getAC_AccountOpening() {
+        
+        return super.getAC_AccountOpening();
+    }
+
+    @Override
+    public component_AC_BalanceUpdates_21_cl getAC_BalanceUpdates() {
+        
+        return super.getAC_BalanceUpdates();
+    }
+
+    @Override
+    public component_AC_HighVolume_17_cl getAC_HighVolume() {
+        
+        return super.getAC_HighVolume();
+    }
+
+    @Override
+    public component_AF_Framework_17_cl getAF_Framework() {
+        
+        return super.getAF_Framework();
+    }
+
+    @Override
+    public String getBASICName() {
+        
+        return super.getBASICName();
+    }
+
+    @Override
+    public String getCompileInfo() {
+        
+        return super.getCompileInfo();
+    }
+
+    @Override
+    public String[] getComponentList() {
+        
+        return super.getComponentList();
+    }
+
+    @Override
+    public jVar getDataStructureFields(java.lang.Object arg0) {
+        
+        return super.getDataStructureFields(arg0);
+    }
+
+    @Override
+    public jVar getDataStructureNames() {
+        
+        return super.getDataStructureNames();
+    }
+
+    @Override
+    public component_EB_API_9_cl getEB_API() {
+        
+        return super.getEB_API();
+    }
+
+    @Override
+    public component_EB_ErrorProcessing_22_cl getEB_ErrorProcessing() {
+        
+        return super.getEB_ErrorProcessing();
+    }
+
+    @Override
+    public component_EB_OverrideProcessing_25_cl getEB_OverrideProcessing() {
+        
+        return super.getEB_OverrideProcessing();
+    }
+
+    @Override
+    public component_EB_Service_15_cl getEB_Service() {
+        
+        return super.getEB_Service();
+    }
+
+    @Override
+    public component_EB_SystemTables_19_cl getEB_SystemTables() {
+        
+        return super.getEB_SystemTables();
+    }
+
+    @Override
+    public component_IN_IbanAPI_12_cl getIN_IbanAPI() {
+        
+        return super.getIN_IbanAPI();
+    }
+
+    @Override
+    public String getImportBasic() {
+        
+        return super.getImportBasic();
+    }
+
+    @Override
+    public component_LI_Config_14_cl getLI_Config() {
+        
+        return super.getLI_Config();
+    }
+
+    @Override
+    public int getNbLines() {
+        
+        return super.getNbLines();
+    }
+
+    @Override
+    public String getPackageBasic() {
+        
+        return super.getPackageBasic();
+    }
+
+    @Override
+    public String[] getParamList() {
+        
+        return super.getParamList();
+    }
+
+    @Override
+    public jVar[] getParams() {
+        
+        return super.getParams();
+    }
+
+    @Override
+    public String getPathFileNameBasic() {
+        
+        return super.getPathFileNameBasic();
+    }
+
+    @Override
+    public String getReplacementInfo() {
+        
+        return super.getReplacementInfo();
+    }
+
+    @Override
+    public component_ST_CompanyCreation_22_cl getST_CompanyCreation() {
+        
+        return super.getST_CompanyCreation();
+    }
+
+    @Override
+    public String[] getVarList() {
+        
+        return super.getVarList();
+    }
+
+    @Override
+    public String getVarValue(String arg0) {
+        
+        return super.getVarValue(arg0);
+    }
+
+    @Override
+    public String getVersion() {
+        
+        return super.getVersion();
+    }
+
+    @Override
+    public void init(jSession arg0) {
+        
+        super.init(arg0);
+    }
+
+    @Override
+    public jVar invoke() {
+        
+        return super.invoke();
+    }
+
+    @Override
+    public jVar invoke(java.lang.Object... arg0) {
+        
+        return super.invoke(arg0);
+    }
+
+    @Override
+    public void keepMoving() {
+        
+        super.keepMoving();
+    }
+
+    @Override
+    protected int lbl_ACCOUNT_OVERDRAWN_ONLINE_UPDATE() {
+        
+        return super.lbl_ACCOUNT_OVERDRAWN_ONLINE_UPDATE();
+    }
+
+    @Override
+    protected int lbl_ACCOUNT_VALIDATION() {
+        
+        return super.lbl_ACCOUNT_VALIDATION();
+    }
+
+    @Override
+    protected int lbl_ASSIGN_ACT_NO() {
+        
+        return super.lbl_ASSIGN_ACT_NO();
+    }
+
+    @Override
+    protected int lbl_CHECK_ACCOUNT_EXISTS() {
+        
+        return super.lbl_CHECK_ACCOUNT_EXISTS();
+    }
+
+    @Override
+    protected int lbl_CHECK_CROSS_POOL_LINK() {
+        
+        return super.lbl_CHECK_CROSS_POOL_LINK();
+    }
+
+    @Override
+    protected int lbl_CHECK_FORWARD_FLAG() {
+        
+        return super.lbl_CHECK_FORWARD_FLAG();
+    }
+
+    @Override
+    protected int lbl_CREATE_ECB() {
+        
+        return super.lbl_CREATE_ECB();
+    }
+
+    @Override
+    protected int lbl_DETERMINE_UPDATE_BUNDLE() {
+        
+        return super.lbl_DETERMINE_UPDATE_BUNDLE();
+    }
+
+    @Override
+    protected int lbl_GET_ACCOUNT_NUMBER() {
+        
+        return super.lbl_GET_ACCOUNT_NUMBER();
+    }
+
+    @Override
+    protected int lbl_GET_ACCOUNT_OVERDRAWN_ID() {
+        
+        return super.lbl_GET_ACCOUNT_OVERDRAWN_ID();
+    }
+
+    @Override
+    protected int lbl_GET_BUNDLE_HIERARCHY_RECORD() {
+        
+        return super.lbl_GET_BUNDLE_HIERARCHY_RECORD();
+    }
+
+    @Override
+    protected int lbl_GET_CHANGED_PROPERTY_CLASS() {
+        
+        return super.lbl_GET_CHANGED_PROPERTY_CLASS();
+    }
+
+    @Override
+    protected int lbl_GET_EXTERNAL_BUNDLE_HIERARCHY_RECORD() {
+        
+        return super.lbl_GET_EXTERNAL_BUNDLE_HIERARCHY_RECORD();
+    }
+
+    @Override
+    protected int lbl_GET_PREVIOUS_RECORD() {
+        
+        return super.lbl_GET_PREVIOUS_RECORD();
+    }
+
+    @Override
+    protected int lbl_GET_UPDATE_ACTIVITY() {
+        
+        return super.lbl_GET_UPDATE_ACTIVITY();
+    }
+
+    @Override
+    protected int lbl_GET_USER_ACTIVITY() {
+        
+        return super.lbl_GET_USER_ACTIVITY();
+    }
+
+    @Override
+    protected int lbl_GetAccountId() {
+        
+        return super.lbl_GetAccountId();
+    }
+
+    @Override
+    protected int lbl_GetChildAccounts() {
+        
+        return super.lbl_GetChildAccounts();
+    }
+
+    @Override
+    protected int lbl_HANDLE_ERROR() {
+        
+        return super.lbl_HANDLE_ERROR();
+    }
+
+    @Override
+    protected int lbl_HANDLE_OVERRIDE() {
+        
+        return super.lbl_HANDLE_OVERRIDE();
+    }
+
+    @Override
+    protected int lbl_HANDLE_PAYOFF_ERROR() {
+        
+        return super.lbl_HANDLE_PAYOFF_ERROR();
+    }
+
+    @Override
+    protected int lbl_INITIALISE() {
+        
+        return super.lbl_INITIALISE();
+    }
+
+    @Override
+    protected int lbl_LIMIT_UPDATE() {
+        
+        return super.lbl_LIMIT_UPDATE();
+    }
+
+    @Override
+    protected int lbl_PROCESS_ACTION() {
+        
+        return super.lbl_PROCESS_ACTION();
+    }
+
+    @Override
+    protected int lbl_PROCESS_AUTHORISE_ACTION() {
+        
+        return super.lbl_PROCESS_AUTHORISE_ACTION();
+    }
+
+    @Override
+    protected int lbl_PROCESS_AUTHORISE_REVERSE_ACTION() {
+        
+        return super.lbl_PROCESS_AUTHORISE_REVERSE_ACTION();
+    }
+
+    @Override
+    protected int lbl_PROCESS_DELETE_ACTION() {
+        
+        return super.lbl_PROCESS_DELETE_ACTION();
+    }
+
+    @Override
+    protected int lbl_PROCESS_DELETE_REVERSE_ACTION() {
+        
+        return super.lbl_PROCESS_DELETE_REVERSE_ACTION();
+    }
+
+    @Override
+    protected int lbl_PROCESS_INPUT_ACTION() {
+        
+        return super.lbl_PROCESS_INPUT_ACTION();
+    }
+
+    @Override
+    protected int lbl_PROCESS_REVERSE_ACTION() {
+        
+        return super.lbl_PROCESS_REVERSE_ACTION();
+    }
+
+    @Override
+    protected int lbl_PROCESS_UPDATE_BUNDLE() {
+        
+        return super.lbl_PROCESS_UPDATE_BUNDLE();
+    }
+
+    @Override
+    protected int lbl_REMOVE_AC_LOCKED_EVENTS() {
+        
+        return super.lbl_REMOVE_AC_LOCKED_EVENTS();
+    }
+
+    @Override
+    protected int lbl_REVERSE_BUNDLE_DETAILS() {
+        
+        return super.lbl_REVERSE_BUNDLE_DETAILS();
+    }
+
+    @Override
+    protected int lbl_SCHEDULE_NEW_ARRANGEMENT_ACTIVITY() {
+        
+        return super.lbl_SCHEDULE_NEW_ARRANGEMENT_ACTIVITY();
+    }
+
+    @Override
+    protected int lbl_SECONDARY_PROCESS_REQUIRD() {
+        
+        return super.lbl_SECONDARY_PROCESS_REQUIRD();
+    }
+
+    @Override
+    protected int lbl_SET_ACTIVITY_DETAILS() {
+        
+        return super.lbl_SET_ACTIVITY_DETAILS();
+    }
+
+    @Override
+    protected int lbl_SPLICE_ALT_IDS() {
+        
+        return super.lbl_SPLICE_ALT_IDS();
+    }
+
+    @Override
+    protected int lbl_TRIGGER_CHANGE_PS() {
+        
+        return super.lbl_TRIGGER_CHANGE_PS();
+    }
+
+    @Override
+    protected int lbl_UPDATE_ACCOUNT() {
+        
+        return super.lbl_UPDATE_ACCOUNT();
+    }
+
+    @Override
+    protected int lbl_UPDATE_ANNIVERSARY_DATE() {
+        
+        return super.lbl_UPDATE_ANNIVERSARY_DATE();
+    }
+
+    @Override
+    protected int lbl_UPDATE_ARRANGEMENT_ALT_ID() {
+        
+        return super.lbl_UPDATE_ARRANGEMENT_ALT_ID();
+    }
+
+    @Override
+    protected int lbl_UPDATE_BALANCE_TREATMENT() {
+        
+        return super.lbl_UPDATE_BALANCE_TREATMENT();
+    }
+
+    @Override
+    protected int lbl_UPDATE_BASE_CURRENCY_RATE() {
+        
+        return super.lbl_UPDATE_BASE_CURRENCY_RATE();
+    }
+
+    @Override
+    protected int lbl_UPDATE_BV_DATES() {
+        
+        return super.lbl_UPDATE_BV_DATES();
+    }
+
+    @Override
+    protected int lbl_UPDATE_CHANGE_CONDITION() {
+        
+        return super.lbl_UPDATE_CHANGE_CONDITION();
+    }
+
+    @Override
+    protected int lbl_UPDATE_EARLY_PROCESSING() {
+        
+        return super.lbl_UPDATE_EARLY_PROCESSING();
+    }
+
+    @Override
+    protected int lbl_UPDATE_OVERDUE_STATUS() {
+        
+        return super.lbl_UPDATE_OVERDUE_STATUS();
+    }
+
+    @Override
+    protected int lbl_UPDATE_POSTING_RESTRICT() {
+        
+        return super.lbl_UPDATE_POSTING_RESTRICT();
+    }
+
+    @Override
+    protected int lbl_UPDATE_SCHEDULED_ACTIVITY() {
+        
+        return super.lbl_UPDATE_SCHEDULED_ACTIVITY();
+    }
+
+    @Override
+    protected int lbl_UPDATE_SECONDARY_ACTIVITY() {
+        
+        return super.lbl_UPDATE_SECONDARY_ACTIVITY();
+    }
+
+    @Override
+    protected int lbl_UPDATE_SERVICE_LIST() {
+        
+        return super.lbl_UPDATE_SERVICE_LIST();
+    }
+
+    @Override
+    protected int main() {
+        
+        return super.main();
+    }
+
+    @Override
+    public void move() {
+        
+        super.move();
+    }
+
+    @Override
+    public void release() {
+        
+        super.release();
+    }
+
+    @Override
+    public void reset() {
+        
+        super.reset();
+    }
+
+    @Override
+    public String setVarValue(String arg0, String arg1) {
+        
+        return super.setVarValue(arg0, arg1);
+    }
+
+    @Override
+    public void stack(AA_ACCOUNT_MAINTAIN_cl arg0) {
+        
+        super.stack(arg0);
+    }
+
+    @Override
+    public void stopMoving() {
+        
+        super.stopMoving();
+    }
+
+}
