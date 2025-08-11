d89920a72643b2b3f74f36fa06ef72a999160478
Alvaro-Camilo-Vargas-Gonzalez <alvaro.camilo@itssglobal.com>
2025-08-11 10:50:04 -0600

    Add files via upload

---
+++ src/com/AaAccountPreCloseCheck.java
@@ -0,0 +1,267 @@
+package com;
+
+import com.temenos.t24.AA_ACCOUNT_PRE_CLOSE_CHECK_cl;
+import com.temenos.t24.component_AA_Framework_17_cl;
+import com.temenos.t24.component_AA_Payoff_14_cl;
+import com.temenos.t24.component_AA_ProductFramework_23_cl;
+import com.temenos.t24.component_AB_Framework_17_cl;
+import com.temenos.t24.component_AC_AccountOpening_21_cl;
+import com.temenos.t24.component_EB_SystemTables_19_cl;
+import com.temenos.tafj.common.jSession;
+import com.temenos.tafj.common.jVar;
+
+public class AaAccountPreCloseCheck extends AA_ACCOUNT_PRE_CLOSE_CHECK_cl {
+
+    @Override
+    protected void CB(int arg0) {
+        
+        super.CB(arg0);
+    }
+
+    @Override
+    public void CLEAR() {
+        
+        super.CLEAR();
+    }
+
+    @Override
+    protected void GOSUB(int arg0, boolean arg1) {
+        
+        super.GOSUB(arg0, arg1);
+    }
+
+    @Override
+    protected void GOSUB(int arg0) {
+        
+        super.GOSUB(arg0);
+    }
+
+    @Override
+    public void create() {
+        
+        super.create();
+    }
+
+    @Override
+    public component_AA_Framework_17_cl getAA_Framework() {
+        
+        return super.getAA_Framework();
+    }
+
+    @Override
+    public component_AA_Payoff_14_cl getAA_Payoff() {
+        
+        return super.getAA_Payoff();
+    }
+
+    @Override
+    public component_AA_ProductFramework_23_cl getAA_ProductFramework() {
+        
+        return super.getAA_ProductFramework();
+    }
+
+    @Override
+    public component_AB_Framework_17_cl getAB_Framework() {
+        
+        return super.getAB_Framework();
+    }
+
+    @Override
+    public component_AC_AccountOpening_21_cl getAC_AccountOpening() {
+        
+        return super.getAC_AccountOpening();
+    }
+
+    @Override
+    public String getBASICName() {
+        
+        return super.getBASICName();
+    }
+
+    @Override
+    public String getCompileInfo() {
+        
+        return super.getCompileInfo();
+    }
+
+    @Override
+    public String[] getComponentList() {
+        
+        return super.getComponentList();
+    }
+
+    @Override
+    public jVar getDataStructureFields(Object arg0) {
+        
+        return super.getDataStructureFields(arg0);
+    }
+
+    @Override
+    public jVar getDataStructureNames() {
+        
+        return super.getDataStructureNames();
+    }
+
+    @Override
+    public component_EB_SystemTables_19_cl getEB_SystemTables() {
+        
+        return super.getEB_SystemTables();
+    }
+
+    @Override
+    public String getImportBasic() {
+        
+        return super.getImportBasic();
+    }
+
+    @Override
+    public int getNbLines() {
+        
+        return super.getNbLines();
+    }
+
+    @Override
+    public String getPackageBasic() {
+        
+        return super.getPackageBasic();
+    }
+
+    @Override
+    public String[] getParamList() {
+        
+        return super.getParamList();
+    }
+
+    @Override
+    public jVar[] getParams() {
+        
+        return super.getParams();
+    }
+
+    @Override
+    public String getPathFileNameBasic() {
+        
+        return super.getPathFileNameBasic();
+    }
+
+    @Override
+    public String getReplacementInfo() {
+        
+        return super.getReplacementInfo();
+    }
+
+    @Override
+    public String[] getVarList() {
+        
+        return super.getVarList();
+    }
+
+    @Override
+    public String getVarValue(String arg0) {
+        
+        return super.getVarValue(arg0);
+    }
+
+    @Override
+    public String getVersion() {
+        
+        return super.getVersion();
+    }
+
+    @Override
+    public void init(jSession arg0) {
+        
+        super.init(arg0);
+    }
+
+    @Override
+    public jVar invoke(Object arg0, Object arg1) {
+        
+        return super.invoke(arg0, arg1);
+    }
+
+    @Override
+    public jVar invoke(Object... arg0) {
+        
+        return super.invoke(arg0);
+    }
+
+    @Override
+    public void keepMoving() {
+        
+        super.keepMoving();
+    }
+
+    @Override
+    protected int lbl_CLOSURE_SCHEDULE_CHECK() {
+        
+        return super.lbl_CLOSURE_SCHEDULE_CHECK();
+    }
+
+    @Override
+    protected int lbl_EXCEPTION_HANDLING() {
+        
+        return super.lbl_EXCEPTION_HANDLING();
+    }
+
+    @Override
+    protected int lbl_INITIALISE() {
+        
+        return super.lbl_INITIALISE();
+    }
+
+    @Override
+    protected int lbl_MAIN_PROCESS() {
+        
+        return super.lbl_MAIN_PROCESS();
+    }
+
+    @Override
+    protected int lbl_PRE_CLOSE_VALIDATION() {
+        
+        return super.lbl_PRE_CLOSE_VALIDATION();
+    }
+
+    @Override
+    protected int main() {
+        
+        return super.main();
+    }
+
+    @Override
+    public void move() {
+        
+        super.move();
+    }
+
+    @Override
+    public void release() {
+        
+        super.release();
+    }
+
+    @Override
+    public void reset() {
+        
+        super.reset();
+    }
+
+    @Override
+    public String setVarValue(String arg0, String arg1) {
+        
+        return super.setVarValue(arg0, arg1);
+    }
+
+    @Override
+    public void stack(AA_ACCOUNT_PRE_CLOSE_CHECK_cl arg0) {
+        
+        super.stack(arg0);
+    }
+
+    @Override
+    public void stopMoving() {
+        
+        super.stopMoving();
+    }
+
+}
