8ee084f40d41f3a26038b83c84a08724b306e799
Alvaro-Camilo-Vargas-Gonzalez <alvaro.camilo@itssglobal.com>
2025-08-13 10:59:29 -0600

    Add files via upload

---
+++ src/com/AaAccountRolloverMakedue.java
@@ -0,0 +1,405 @@
+package com;
+
+import com.temenos.t24.AA_ACCOUNT_ROLLOVER_MAKEDUE_cl;
+import com.temenos.t24.component_AA_Account_15_cl;
+import com.temenos.t24.component_AA_Accounting_18_cl;
+import com.temenos.t24.component_AA_Framework_17_cl;
+import com.temenos.t24.component_AA_Participant_19_cl;
+import com.temenos.t24.component_AA_PaymentSchedule_22_cl;
+import com.temenos.t24.component_AA_ProductFramework_23_cl;
+import com.temenos.t24.component_AA_SplitsMerges_19_cl;
+import com.temenos.t24.component_AC_BalanceUpdates_21_cl;
+import com.temenos.t24.component_AC_SoftAccounting_21_cl;
+import com.temenos.t24.component_AF_Framework_17_cl;
+import com.temenos.t24.component_EB_ErrorProcessing_22_cl;
+import com.temenos.t24.component_EB_SystemTables_19_cl;
+import com.temenos.tafj.common.jSession;
+import com.temenos.tafj.common.jVar;
+
+public class AaAccountRolloverMakedue extends AA_ACCOUNT_ROLLOVER_MAKEDUE_cl {
+
+    @Override
+    protected void CB(int arg0) {
+        
+        super.CB(arg0);
+    }
+
+    @Override
+    public void CLEAR() {
+        
+        super.CLEAR();
+    }
+
+    @Override
+    protected void GOSUB(int arg0, boolean arg1) {
+        
+        super.GOSUB(arg0, arg1);
+    }
+
+    @Override
+    protected void GOSUB(int arg0) {
+        
+        super.GOSUB(arg0);
+    }
+
+    @Override
+    public void create() {
+        
+        super.create();
+    }
+
+    @Override
+    public component_AA_Account_15_cl getAA_Account() {
+        
+        return super.getAA_Account();
+    }
+
+    @Override
+    public component_AA_Accounting_18_cl getAA_Accounting() {
+        
+        return super.getAA_Accounting();
+    }
+
+    @Override
+    public component_AA_Framework_17_cl getAA_Framework() {
+        
+        return super.getAA_Framework();
+    }
+
+    @Override
+    public component_AA_Participant_19_cl getAA_Participant() {
+        
+        return super.getAA_Participant();
+    }
+
+    @Override
+    public component_AA_PaymentSchedule_22_cl getAA_PaymentSchedule() {
+        
+        return super.getAA_PaymentSchedule();
+    }
+
+    @Override
+    public component_AA_ProductFramework_23_cl getAA_ProductFramework() {
+        
+        return super.getAA_ProductFramework();
+    }
+
+    @Override
+    public component_AA_SplitsMerges_19_cl getAA_SplitsMerges() {
+        
+        return super.getAA_SplitsMerges();
+    }
+
+    @Override
+    public component_AC_BalanceUpdates_21_cl getAC_BalanceUpdates() {
+        
+        return super.getAC_BalanceUpdates();
+    }
+
+    @Override
+    public component_AC_SoftAccounting_21_cl getAC_SoftAccounting() {
+        
+        return super.getAC_SoftAccounting();
+    }
+
+    @Override
+    public component_AF_Framework_17_cl getAF_Framework() {
+        
+        return super.getAF_Framework();
+    }
+
+    @Override
+    public String getBASICName() {
+        
+        return super.getBASICName();
+    }
+
+    @Override
+    public String getCompileInfo() {
+        
+        return super.getCompileInfo();
+    }
+
+    @Override
+    public String[] getComponentList() {
+        
+        return super.getComponentList();
+    }
+
+    @Override
+    public jVar getDataStructureFields(Object arg0) {
+        
+        return super.getDataStructureFields(arg0);
+    }
+
+    @Override
+    public jVar getDataStructureNames() {
+        
+        return super.getDataStructureNames();
+    }
+
+    @Override
+    public component_EB_ErrorProcessing_22_cl getEB_ErrorProcessing() {
+        
+        return super.getEB_ErrorProcessing();
+    }
+
+    @Override
+    public component_EB_SystemTables_19_cl getEB_SystemTables() {
+        
+        return super.getEB_SystemTables();
+    }
+
+    @Override
+    public String getImportBasic() {
+        
+        return super.getImportBasic();
+    }
+
+    @Override
+    public int getNbLines() {
+        
+        return super.getNbLines();
+    }
+
+    @Override
+    public String getPackageBasic() {
+        
+        return super.getPackageBasic();
+    }
+
+    @Override
+    public String[] getParamList() {
+        
+        return super.getParamList();
+    }
+
+    @Override
+    public jVar[] getParams() {
+        
+        return super.getParams();
+    }
+
+    @Override
+    public String getPathFileNameBasic() {
+        
+        return super.getPathFileNameBasic();
+    }
+
+    @Override
+    public String getReplacementInfo() {
+        
+        return super.getReplacementInfo();
+    }
+
+    @Override
+    public String[] getVarList() {
+        
+        return super.getVarList();
+    }
+
+    @Override
+    public String getVarValue(String arg0) {
+        
+        return super.getVarValue(arg0);
+    }
+
+    @Override
+    public String getVersion() {
+        
+        return super.getVersion();
+    }
+
+    @Override
+    public void init(jSession arg0) {
+        
+        super.init(arg0);
+    }
+
+    @Override
+    public jVar invoke() {
+        
+        return super.invoke();
+    }
+
+    @Override
+    public jVar invoke(Object... arg0) {
+        
+        return super.invoke(arg0);
+    }
+
+    @Override
+    public void keepMoving() {
+        
+        super.keepMoving();
+    }
+
+    @Override
+    protected int lbl_BuildAccountingUpdates() {
+        
+        return super.lbl_BuildAccountingUpdates();
+    }
+
+    @Override
+    protected int lbl_GetBalanceType() {
+        
+        return super.lbl_GetBalanceType();
+    }
+
+    @Override
+    protected int lbl_GetBillDetails() {
+        
+        return super.lbl_GetBillDetails();
+    }
+
+    @Override
+    protected int lbl_GetCurBalance() {
+        
+        return super.lbl_GetCurBalance();
+    }
+
+    @Override
+    protected int lbl_GetParticipantsDetails() {
+        
+        return super.lbl_GetParticipantsDetails();
+    }
+
+    @Override
+    protected int lbl_GetPropertyRecord() {
+        
+        return super.lbl_GetPropertyRecord();
+    }
+
+    @Override
+    protected int lbl_GetSourceRolloverAmount() {
+        
+        return super.lbl_GetSourceRolloverAmount();
+    }
+
+    @Override
+    protected int lbl_HandleError() {
+        
+        return super.lbl_HandleError();
+    }
+
+    @Override
+    protected int lbl_Initialise() {
+        
+        return super.lbl_Initialise();
+    }
+
+    @Override
+    protected int lbl_MakeBillDue() {
+        
+        return super.lbl_MakeBillDue();
+    }
+
+    @Override
+    protected int lbl_ProcessAccountingUpdates() {
+        
+        return super.lbl_ProcessAccountingUpdates();
+    }
+
+    @Override
+    protected int lbl_ProcessAction() {
+        
+        return super.lbl_ProcessAction();
+    }
+
+    @Override
+    protected int lbl_ProcessAuthorise() {
+        
+        return super.lbl_ProcessAuthorise();
+    }
+
+    @Override
+    protected int lbl_ProcessDelete() {
+        
+        return super.lbl_ProcessDelete();
+    }
+
+    @Override
+    protected int lbl_ProcessInput() {
+        
+        return super.lbl_ProcessInput();
+    }
+
+    @Override
+    protected int lbl_ProcessParticipantsAction() {
+        
+        return super.lbl_ProcessParticipantsAction();
+    }
+
+    @Override
+    protected int lbl_ProcessReversal() {
+        
+        return super.lbl_ProcessReversal();
+    }
+
+    @Override
+    protected int lbl_SetActivityDetails() {
+        
+        return super.lbl_SetActivityDetails();
+    }
+
+    @Override
+    protected int lbl_UpdateBillStatus() {
+        
+        return super.lbl_UpdateBillStatus();
+    }
+
+    @Override
+    protected int lbl_UpdateSecondaryActivity() {
+        
+        return super.lbl_UpdateSecondaryActivity();
+    }
+
+    @Override
+    protected int lbl_WriteBillDetails() {
+        
+        return super.lbl_WriteBillDetails();
+    }
+
+    @Override
+    protected int main() {
+        
+        return super.main();
+    }
+
+    @Override
+    public void move() {
+        
+        super.move();
+    }
+
+    @Override
+    public void release() {
+        
+        super.release();
+    }
+
+    @Override
+    public void reset() {
+        
+        super.reset();
+    }
+
+    @Override
+    public String setVarValue(String arg0, String arg1) {
+        
+        return super.setVarValue(arg0, arg1);
+    }
+
+    @Override
+    public void stack(AA_ACCOUNT_ROLLOVER_MAKEDUE_cl arg0) {
+        
+        super.stack(arg0);
+    }
+
+    @Override
+    public void stopMoving() {
+        
+        super.stopMoving();
+    }
+
+}
