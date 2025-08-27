2eb74ad2efee3389eae02a9a99ec79f16704c576
Alvaro-Camilo-Vargas-Gonzalez <alvaro.camilo@itssglobal.com>
2025-08-27 12:08:00 -0500

    Add files via upload

---
+++ src/com/AaActivityAccountingIntegrityCheck.java
@@ -0,0 +1,329 @@
+package com;
+
+import com.temenos.t24.AA_ACTIVITY_ACCOUNTING_INTEGRITY_CHECK_cl;
+import com.temenos.t24.component_AA_Framework_17_cl;
+import com.temenos.t24.component_AA_Interfaces_18_cl;
+import com.temenos.t24.component_AA_ProductFramework_23_cl;
+import com.temenos.t24.component_AC_SoftAccounting_21_cl;
+import com.temenos.t24.component_AF_Framework_17_cl;
+import com.temenos.t24.component_EB_DataAccess_17_cl;
+import com.temenos.t24.component_EB_SystemTables_19_cl;
+import com.temenos.t24.component_EB_Template_16_cl;
+import com.temenos.tafj.common.jSession;
+import com.temenos.tafj.common.jVar;
+
+public class AaActivityAccountingIntegrityCheck extends AA_ACTIVITY_ACCOUNTING_INTEGRITY_CHECK_cl {
+
+    @Override
+    protected void CB(int arg0) {
+        
+        super.CB(arg0);
+    }
+
+    @Override
+    public void CLEAR() {
+        
+        super.CLEAR();
+    }
+
+    @Override
+    protected void GOSUB(int arg0, boolean arg1) {
+        
+        super.GOSUB(arg0, arg1);
+    }
+
+    @Override
+    protected void GOSUB(int arg0) {
+        
+        super.GOSUB(arg0);
+    }
+
+    @Override
+    public void create() {
+        
+        super.create();
+    }
+
+    @Override
+    public component_AA_Framework_17_cl getAA_Framework() {
+        
+        return super.getAA_Framework();
+    }
+
+    @Override
+    public component_AA_Interfaces_18_cl getAA_Interfaces() {
+        
+        return super.getAA_Interfaces();
+    }
+
+    @Override
+    public component_AA_ProductFramework_23_cl getAA_ProductFramework() {
+        
+        return super.getAA_ProductFramework();
+    }
+
+    @Override
+    public component_AC_SoftAccounting_21_cl getAC_SoftAccounting() {
+        
+        return super.getAC_SoftAccounting();
+    }
+
+    @Override
+    public component_AF_Framework_17_cl getAF_Framework() {
+        
+        return super.getAF_Framework();
+    }
+
+    @Override
+    public String getBASICName() {
+        
+        return super.getBASICName();
+    }
+
+    @Override
+    public String getCompileInfo() {
+        
+        return super.getCompileInfo();
+    }
+
+    @Override
+    public String[] getComponentList() {
+        
+        return super.getComponentList();
+    }
+
+    @Override
+    public jVar getDataStructureFields(Object arg0) {
+        
+        return super.getDataStructureFields(arg0);
+    }
+
+    @Override
+    public jVar getDataStructureNames() {
+        
+        return super.getDataStructureNames();
+    }
+
+    @Override
+    public component_EB_DataAccess_17_cl getEB_DataAccess() {
+        
+        return super.getEB_DataAccess();
+    }
+
+    @Override
+    public component_EB_SystemTables_19_cl getEB_SystemTables() {
+        
+        return super.getEB_SystemTables();
+    }
+
+    @Override
+    public component_EB_Template_16_cl getEB_Template() {
+        
+        return super.getEB_Template();
+    }
+
+    @Override
+    public String getImportBasic() {
+        
+        return super.getImportBasic();
+    }
+
+    @Override
+    public int getNbLines() {
+        
+        return super.getNbLines();
+    }
+
+    @Override
+    public String getPackageBasic() {
+        
+        return super.getPackageBasic();
+    }
+
+    @Override
+    public String[] getParamList() {
+        
+        return super.getParamList();
+    }
+
+    @Override
+    public jVar[] getParams() {
+        
+        return super.getParams();
+    }
+
+    @Override
+    public String getPathFileNameBasic() {
+        
+        return super.getPathFileNameBasic();
+    }
+
+    @Override
+    public String getReplacementInfo() {
+        
+        return super.getReplacementInfo();
+    }
+
+    @Override
+    public String[] getVarList() {
+        
+        return super.getVarList();
+    }
+
+    @Override
+    public String getVarValue(String arg0) {
+        
+        return super.getVarValue(arg0);
+    }
+
+    @Override
+    public String getVersion() {
+        
+        return super.getVersion();
+    }
+
+    @Override
+    public void init(jSession arg0) {
+        
+        super.init(arg0);
+    }
+
+    @Override
+    public jVar invoke(Object arg0, Object arg1, Object arg2, Object arg3) {
+        
+        return super.invoke(arg0, arg1, arg2, arg3);
+    }
+
+    @Override
+    public jVar invoke(Object... arg0) {
+        
+        return super.invoke(arg0);
+    }
+
+    @Override
+    public void keepMoving() {
+        
+        super.keepMoving();
+    }
+
+    @Override
+    protected int lbl_CHECK_FOR_SUSPPAYMENT() {
+        
+        return super.lbl_CHECK_FOR_SUSPPAYMENT();
+    }
+
+    @Override
+    protected int lbl_CHECK_IGNORE_TYPES() {
+        
+        return super.lbl_CHECK_IGNORE_TYPES();
+    }
+
+    @Override
+    protected int lbl_CHECK_INVALID_BALANCE_TYPE() {
+        
+        return super.lbl_CHECK_INVALID_BALANCE_TYPE();
+    }
+
+    @Override
+    protected int lbl_CHECK_MEMO_ONLY_PROP() {
+        
+        return super.lbl_CHECK_MEMO_ONLY_PROP();
+    }
+
+    @Override
+    protected int lbl_FORM_DETAILS() {
+        
+        return super.lbl_FORM_DETAILS();
+    }
+
+    @Override
+    protected int lbl_GET_ECB_CHECKS_REQUIRED() {
+        
+        return super.lbl_GET_ECB_CHECKS_REQUIRED();
+    }
+
+    @Override
+    protected int lbl_GET_PRE_POST_PROPERTY_BALANCES() {
+        
+        return super.lbl_GET_PRE_POST_PROPERTY_BALANCES();
+    }
+
+    @Override
+    protected int lbl_GET_PROPERTY_DETAILS() {
+        
+        return super.lbl_GET_PROPERTY_DETAILS();
+    }
+
+    @Override
+    protected int lbl_INITIALISE() {
+        
+        return super.lbl_INITIALISE();
+    }
+
+    @Override
+    protected int lbl_RETRIEVE_PRE_POST_DETAILS() {
+        
+        return super.lbl_RETRIEVE_PRE_POST_DETAILS();
+    }
+
+    @Override
+    protected int lbl_STORE_PROPERTY_DETAILS() {
+        
+        return super.lbl_STORE_PROPERTY_DETAILS();
+    }
+
+    @Override
+    protected int lbl_exceptionTypes() {
+        
+        return super.lbl_exceptionTypes();
+    }
+
+    @Override
+    protected int lbl_getoverduestatus() {
+        
+        return super.lbl_getoverduestatus();
+    }
+
+    @Override
+    protected int main() {
+        
+        return super.main();
+    }
+
+    @Override
+    public void move() {
+        
+        super.move();
+    }
+
+    @Override
+    public void release() {
+        
+        super.release();
+    }
+
+    @Override
+    public void reset() {
+        
+        super.reset();
+    }
+
+    @Override
+    public String setVarValue(String arg0, String arg1) {
+        
+        return super.setVarValue(arg0, arg1);
+    }
+
+    @Override
+    public void stack(AA_ACTIVITY_ACCOUNTING_INTEGRITY_CHECK_cl arg0) {
+        
+        super.stack(arg0);
+    }
+
+    @Override
+    public void stopMoving() {
+        
+        super.stopMoving();
+    }
+
+}
