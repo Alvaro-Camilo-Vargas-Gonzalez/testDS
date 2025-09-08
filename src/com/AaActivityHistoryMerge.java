5dc746488d138a1fd8d179bbc0b908b6094551ae
Alvaro-Camilo-Vargas-Gonzalez <alvaro.camilo@itssglobal.com>
2025-09-08 10:29:22 -0500

    Add files via upload

---
+++ src/com/AaActivityHistoryMerge.java
@@ -0,0 +1,403 @@
+package com;
+
+import com.temenos.t24.AA_ACTIVITY_HISTORY_MERGE_cl;
+import com.temenos.t24.component_AA_Framework_17_cl;
+import com.temenos.t24.component_AC_HighVolume_17_cl;
+import com.temenos.t24.component_EB_Service_15_cl;
+import com.temenos.t24.component_EB_SystemTables_19_cl;
+import com.temenos.tafj.common.jSession;
+import com.temenos.tafj.common.jVar;
+
+public class AaActivityHistoryMerge extends AA_ACTIVITY_HISTORY_MERGE_cl {
+
+    @Override
+    protected void CB(int arg0) {
+        
+        super.CB(arg0);
+    }
+
+    @Override
+    public void CLEAR() {
+        
+        super.CLEAR();
+    }
+
+    @Override
+    protected void GOSUB(int arg0, boolean arg1) {
+        
+        super.GOSUB(arg0, arg1);
+    }
+
+    @Override
+    protected void GOSUB(int arg0) {
+        
+        super.GOSUB(arg0);
+    }
+
+    @Override
+    public void create() {
+        
+        super.create();
+    }
+
+    @Override
+    public component_AA_Framework_17_cl getAA_Framework() {
+        
+        return super.getAA_Framework();
+    }
+
+    @Override
+    public component_AC_HighVolume_17_cl getAC_HighVolume() {
+        
+        return super.getAC_HighVolume();
+    }
+
+    @Override
+    public String getBASICName() {
+        
+        return super.getBASICName();
+    }
+
+    @Override
+    public String getCompileInfo() {
+        
+        return super.getCompileInfo();
+    }
+
+    @Override
+    public String[] getComponentList() {
+        
+        return super.getComponentList();
+    }
+
+    @Override
+    public jVar getDataStructureFields(Object arg0) {
+        
+        return super.getDataStructureFields(arg0);
+    }
+
+    @Override
+    public jVar getDataStructureNames() {
+        
+        return super.getDataStructureNames();
+    }
+
+    @Override
+    public component_EB_Service_15_cl getEB_Service() {
+        
+        return super.getEB_Service();
+    }
+
+    @Override
+    public component_EB_SystemTables_19_cl getEB_SystemTables() {
+        
+        return super.getEB_SystemTables();
+    }
+
+    @Override
+    public String getImportBasic() {
+        
+        return super.getImportBasic();
+    }
+
+    @Override
+    public int getNbLines() {
+        
+        return super.getNbLines();
+    }
+
+    @Override
+    public String getPackageBasic() {
+        
+        return super.getPackageBasic();
+    }
+
+    @Override
+    public String[] getParamList() {
+        
+        return super.getParamList();
+    }
+
+    @Override
+    public jVar[] getParams() {
+        
+        return super.getParams();
+    }
+
+    @Override
+    public String getPathFileNameBasic() {
+        
+        return super.getPathFileNameBasic();
+    }
+
+    @Override
+    public String getReplacementInfo() {
+        
+        return super.getReplacementInfo();
+    }
+
+    @Override
+    public String[] getVarList() {
+        
+        return super.getVarList();
+    }
+
+    @Override
+    public String getVarValue(String arg0) {
+        
+        return super.getVarValue(arg0);
+    }
+
+    @Override
+    public String getVersion() {
+        
+        return super.getVersion();
+    }
+
+    @Override
+    public void init(jSession arg0) {
+        
+        super.init(arg0);
+    }
+
+    @Override
+    public jVar invoke(Object arg0, Object arg1, Object arg2, Object arg3, Object arg4, Object arg5) {
+        
+        return super.invoke(arg0, arg1, arg2, arg3, arg4, arg5);
+    }
+
+    @Override
+    public jVar invoke(Object... arg0) {
+        
+        return super.invoke(arg0);
+    }
+
+    @Override
+    public void keepMoving() {
+        
+        super.keepMoving();
+    }
+
+    @Override
+    protected int lbl_CheckActivityStatus() {
+        
+        return super.lbl_CheckActivityStatus();
+    }
+
+    @Override
+    protected int lbl_CheckReferenceDetails() {
+        
+        return super.lbl_CheckReferenceDetails();
+    }
+
+    @Override
+    protected int lbl_DelExternalRefDetails() {
+        
+        return super.lbl_DelExternalRefDetails();
+    }
+
+    @Override
+    protected int lbl_DeleteReferenceDetails() {
+        
+        return super.lbl_DeleteReferenceDetails();
+    }
+
+    @Override
+    protected int lbl_DoAuthoriseProcess() {
+        
+        return super.lbl_DoAuthoriseProcess();
+    }
+
+    @Override
+    protected int lbl_DoUnauthoriseProcess() {
+        
+        return super.lbl_DoUnauthoriseProcess();
+    }
+
+    @Override
+    protected int lbl_FillActivityDetails() {
+        
+        return super.lbl_FillActivityDetails();
+    }
+
+    @Override
+    protected int lbl_FindAcctivityRefCnt() {
+        
+        return super.lbl_FindAcctivityRefCnt();
+    }
+
+    @Override
+    protected int lbl_FindEffectiveDateCnt() {
+        
+        return super.lbl_FindEffectiveDateCnt();
+    }
+
+    @Override
+    protected int lbl_GetActivityHistoryRecord() {
+        
+        return super.lbl_GetActivityHistoryRecord();
+    }
+
+    @Override
+    protected int lbl_GetActivityRecord() {
+        
+        return super.lbl_GetActivityRecord();
+    }
+
+    @Override
+    protected int lbl_GetContextValue() {
+        
+        return super.lbl_GetContextValue();
+    }
+
+    @Override
+    protected int lbl_GetProcessType() {
+        
+        return super.lbl_GetProcessType();
+    }
+
+    @Override
+    protected int lbl_Initialise() {
+        
+        return super.lbl_Initialise();
+    }
+
+    @Override
+    protected int lbl_InitiateMergingProcess() {
+        
+        return super.lbl_InitiateMergingProcess();
+    }
+
+    @Override
+    protected int lbl_InsertActivityPosition() {
+        
+        return super.lbl_InsertActivityPosition();
+    }
+
+    @Override
+    protected int lbl_InsertNewDate() {
+        
+        return super.lbl_InsertNewDate();
+    }
+
+    @Override
+    protected int lbl_LocateActivityReference() {
+        
+        return super.lbl_LocateActivityReference();
+    }
+
+    @Override
+    protected int lbl_Process() {
+        
+        return super.lbl_Process();
+    }
+
+    @Override
+    protected int lbl_ProcessActivityArchive() {
+        
+        return super.lbl_ProcessActivityArchive();
+    }
+
+    @Override
+    protected int lbl_ProcessMerging() {
+        
+        return super.lbl_ProcessMerging();
+    }
+
+    @Override
+    protected int lbl_ProcessReferenceDetailsMerging() {
+        
+        return super.lbl_ProcessReferenceDetailsMerging();
+    }
+
+    @Override
+    protected int lbl_ReadFiles() {
+        
+        return super.lbl_ReadFiles();
+    }
+
+    @Override
+    protected int lbl_UpdateActivityDetails() {
+        
+        return super.lbl_UpdateActivityDetails();
+    }
+
+    @Override
+    protected int lbl_UpdateBillDetails() {
+        
+        return super.lbl_UpdateBillDetails();
+    }
+
+    @Override
+    protected int lbl_UpdateDdDetails() {
+        
+        return super.lbl_UpdateDdDetails();
+    }
+
+    @Override
+    protected int lbl_UpdateExternalReferenceDetails() {
+        
+        return super.lbl_UpdateExternalReferenceDetails();
+    }
+
+    @Override
+    protected int lbl_UpdatePoDetails() {
+        
+        return super.lbl_UpdatePoDetails();
+    }
+
+    @Override
+    protected int lbl_UpdateReferenceDetails() {
+        
+        return super.lbl_UpdateReferenceDetails();
+    }
+
+    @Override
+    protected int lbl_WriteActivityHistoryRecord() {
+        
+        return super.lbl_WriteActivityHistoryRecord();
+    }
+
+    @Override
+    protected int main() {
+        
+        return super.main();
+    }
+
+    @Override
+    public void move() {
+        
+        super.move();
+    }
+
+    @Override
+    public void release() {
+        
+        super.release();
+    }
+
+    @Override
+    public void reset() {
+        
+        super.reset();
+    }
+
+    @Override
+    public String setVarValue(String arg0, String arg1) {
+        
+        return super.setVarValue(arg0, arg1);
+    }
+
+    @Override
+    public void stack(AA_ACTIVITY_HISTORY_MERGE_cl arg0) {
+        
+        super.stack(arg0);
+    }
+
+    @Override
+    public void stopMoving() {
+        
+        super.stopMoving();
+    }
+
+}
