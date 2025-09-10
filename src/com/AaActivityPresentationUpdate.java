f5a76bcb06b48b35c7197ffc9bbbae4c29f42059
Alvaro-Camilo-Vargas-Gonzalez <alvaro.camilo@itssglobal.com>
2025-09-10 13:55:43 -0500

    Add files via upload

--- src/com/AaActivityPresentationUpdate.java
+++ src/com/AaActivityPresentationUpdate.java
@@ -13,193 +13,193 @@ public class AaActivityPresentationUpdate extends AA_ACTIVITY_PRESENTATION_UPDAT
 
     @Override
     protected void CB(int arg0) {
-        // TODO Auto-generated method stub
+        
         super.CB(arg0);
     }
 
     @Override
     public void CLEAR() {
-        // TODO Auto-generated method stub
+        
         super.CLEAR();
     }
 
     @Override
     protected void GOSUB(int arg0, boolean arg1) {
-        // TODO Auto-generated method stub
+        
         super.GOSUB(arg0, arg1);
     }
 
     @Override
     protected void GOSUB(int arg0) {
-        // TODO Auto-generated method stub
+        
         super.GOSUB(arg0);
     }
 
     @Override
     public void create() {
-        // TODO Auto-generated method stub
+        
         super.create();
     }
 
     @Override
     public component_AA_ActivityPresentation_27_cl getAA_ActivityPresentation() {
-        // TODO Auto-generated method stub
+        
         return super.getAA_ActivityPresentation();
     }
 
     @Override
     public component_AA_Framework_17_cl getAA_Framework() {
-        // TODO Auto-generated method stub
+        
         return super.getAA_Framework();
     }
 
     @Override
     public component_AF_Framework_17_cl getAF_Framework() {
-        // TODO Auto-generated method stub
+        
         return super.getAF_Framework();
     }
 
     @Override
     public String getBASICName() {
-        // TODO Auto-generated method stub
+        
         return super.getBASICName();
     }
 
     @Override
     public String getCompileInfo() {
-        // TODO Auto-generated method stub
+        
         return super.getCompileInfo();
     }
 
     @Override
     public String[] getComponentList() {
-        // TODO Auto-generated method stub
+        
         return super.getComponentList();
     }
 
     @Override
     public jVar getDataStructureFields(Object arg0) {
-        // TODO Auto-generated method stub
+        
         return super.getDataStructureFields(arg0);
     }
 
     @Override
     public jVar getDataStructureNames() {
-        // TODO Auto-generated method stub
+        
         return super.getDataStructureNames();
     }
 
     @Override
     public component_EB_ErrorProcessing_22_cl getEB_ErrorProcessing() {
-        // TODO Auto-generated method stub
+        
         return super.getEB_ErrorProcessing();
     }
 
     @Override
     public component_EB_SystemTables_19_cl getEB_SystemTables() {
-        // TODO Auto-generated method stub
+        
         return super.getEB_SystemTables();
     }
 
     @Override
     public String getImportBasic() {
-        // TODO Auto-generated method stub
+        
         return super.getImportBasic();
     }
 
     @Override
     public int getNbLines() {
-        // TODO Auto-generated method stub
+        
         return super.getNbLines();
     }
 
     @Override
     public String getPackageBasic() {
-        // TODO Auto-generated method stub
+        
         return super.getPackageBasic();
     }
 
     @Override
     public String[] getParamList() {
-        // TODO Auto-generated method stub
+        
         return super.getParamList();
     }
 
     @Override
     public jVar[] getParams() {
-        // TODO Auto-generated method stub
+        
         return super.getParams();
     }
 
     @Override
     public String getPathFileNameBasic() {
-        // TODO Auto-generated method stub
+        
         return super.getPathFileNameBasic();
     }
 
     @Override
     public String getReplacementInfo() {
-        // TODO Auto-generated method stub
+        
         return super.getReplacementInfo();
     }
 
     @Override
     public String[] getVarList() {
-        // TODO Auto-generated method stub
+        
         return super.getVarList();
     }
 
     @Override
     public String getVarValue(String arg0) {
-        // TODO Auto-generated method stub
+        
         return super.getVarValue(arg0);
     }
 
     @Override
     public String getVersion() {
-        // TODO Auto-generated method stub
+        
         return super.getVersion();
     }
 
     @Override
     public void init(jSession arg0) {
-        // TODO Auto-generated method stub
+        
         super.init(arg0);
     }
 
     @Override
     public jVar invoke() {
-        // TODO Auto-generated method stub
+        
         return super.invoke();
     }
 
     @Override
     public jVar invoke(Object... arg0) {
-        // TODO Auto-generated method stub
+        
         return super.invoke(arg0);
     }
 
     @Override
     public void keepMoving() {
-        // TODO Auto-generated method stub
+        
         super.keepMoving();
     }
 
     @Override
     protected int lbl_HANDLE_ERROR() {
-        // TODO Auto-generated method stub
+        
         return super.lbl_HANDLE_ERROR();
     }
 
     @Override
     protected int lbl_INITIALISE() {
-        // TODO Auto-generated method stub
+        
         return super.lbl_INITIALISE();
     }
 
     @Override
     protected int lbl_PROCESS_ACTION() {
-        // TODO Auto-generated method stub
+        
         return super.lbl_PROCESS_ACTION();
     }
 
