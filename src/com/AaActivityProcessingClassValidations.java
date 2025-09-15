a850d74e373563e853ac1c8ae619af81651a270d
Alvaro-Camilo-Vargas-Gonzalez <alvaro.camilo@itssglobal.com>
2025-09-15 13:03:29 -0500

    Add files via upload

---
+++ src/com/AaActivityProcessingClassValidations.java
@@ -0,0 +1,309 @@
+package com;
+
+import com.temenos.t24.AA_ACTIVITY_PROCESSING_CLASS_VALIDATIONS_cl;
+import com.temenos.t24.component_AF_ActivityProcessing_25_cl;
+import com.temenos.t24.component_AF_ClassFramework_21_cl;
+import com.temenos.t24.component_AF_Framework_17_cl;
+import com.temenos.t24.component_EB_ErrorProcessing_22_cl;
+import com.temenos.t24.component_EB_SystemTables_19_cl;
+import com.temenos.t24.component_EB_Template_16_cl;
+import com.temenos.tafj.common.jSession;
+import com.temenos.tafj.common.jVar;
+
+public class AaActivityProcessingClassValidations extends AA_ACTIVITY_PROCESSING_CLASS_VALIDATIONS_cl {
+
+    @Override
+    protected void CB(int arg0) {
+        
+        super.CB(arg0);
+    }
+
+    @Override
+    public void CLEAR() {
+        
+        super.CLEAR();
+    }
+
+    @Override
+    protected void GOSUB(int arg0, boolean arg1) {
+        
+        super.GOSUB(arg0, arg1);
+    }
+
+    @Override
+    protected void GOSUB(int arg0) {
+        
+        super.GOSUB(arg0);
+    }
+
+    @Override
+    public void create() {
+        
+        super.create();
+    }
+
+    @Override
+    public component_AF_ActivityProcessing_25_cl getAF_ActivityProcessing() {
+        
+        return super.getAF_ActivityProcessing();
+    }
+
+    @Override
+    public component_AF_ClassFramework_21_cl getAF_ClassFramework() {
+        
+        return super.getAF_ClassFramework();
+    }
+
+    @Override
+    public component_AF_Framework_17_cl getAF_Framework() {
+        
+        return super.getAF_Framework();
+    }
+
+    @Override
+    public String getBASICName() {
+        
+        return super.getBASICName();
+    }
+
+    @Override
+    public String getCompileInfo() {
+        
+        return super.getCompileInfo();
+    }
+
+    @Override
+    public String[] getComponentList() {
+        
+        return super.getComponentList();
+    }
+
+    @Override
+    public jVar getDataStructureFields(Object arg0) {
+        
+        return super.getDataStructureFields(arg0);
+    }
+
+    @Override
+    public jVar getDataStructureNames() {
+        
+        return super.getDataStructureNames();
+    }
+
+    @Override
+    public component_EB_ErrorProcessing_22_cl getEB_ErrorProcessing() {
+        
+        return super.getEB_ErrorProcessing();
+    }
+
+    @Override
+    public component_EB_SystemTables_19_cl getEB_SystemTables() {
+        
+        return super.getEB_SystemTables();
+    }
+
+    @Override
+    public component_EB_Template_16_cl getEB_Template() {
+        
+        return super.getEB_Template();
+    }
+
+    @Override
+    public String getImportBasic() {
+        
+        return super.getImportBasic();
+    }
+
+    @Override
+    public int getNbLines() {
+        
+        return super.getNbLines();
+    }
+
+    @Override
+    public String getPackageBasic() {
+        
+        return super.getPackageBasic();
+    }
+
+    @Override
+    public String[] getParamList() {
+        
+        return super.getParamList();
+    }
+
+    @Override
+    public jVar[] getParams() {
+        
+        return super.getParams();
+    }
+
+    @Override
+    public String getPathFileNameBasic() {
+        
+        return super.getPathFileNameBasic();
+    }
+
+    @Override
+    public String getReplacementInfo() {
+        
+        return super.getReplacementInfo();
+    }
+
+    @Override
+    public String[] getVarList() {
+        
+        return super.getVarList();
+    }
+
+    @Override
+    public String getVarValue(String arg0) {
+        
+        return super.getVarValue(arg0);
+    }
+
+    @Override
+    public String getVersion() {
+        
+        return super.getVersion();
+    }
+
+    @Override
+    public void init(jSession arg0) {
+        
+        super.init(arg0);
+    }
+
+    @Override
+    public jVar invoke() {
+        
+        return super.invoke();
+    }
+
+    @Override
+    public jVar invoke(Object... arg0) {
+        
+        return super.invoke(arg0);
+    }
+
+    @Override
+    public void keepMoving() {
+        
+        super.keepMoving();
+    }
+
+    @Override
+    protected int lbl_CHECK_ACTIVITY_FIELD_INTEGRITY() {
+        
+        return super.lbl_CHECK_ACTIVITY_FIELD_INTEGRITY();
+    }
+
+    @Override
+    protected int lbl_DESIGNER_CROSSVAL() {
+        
+        return super.lbl_DESIGNER_CROSSVAL();
+    }
+
+    @Override
+    protected int lbl_GET_FIELD_VALUES() {
+        
+        return super.lbl_GET_FIELD_VALUES();
+    }
+
+    @Override
+    protected int lbl_INITIALISE() {
+        
+        return super.lbl_INITIALISE();
+    }
+
+    @Override
+    protected int lbl_INITIALISE_ERROR_VARIABLES() {
+        
+        return super.lbl_INITIALISE_ERROR_VARIABLES();
+    }
+
+    @Override
+    protected int lbl_MAIN_PROCESS() {
+        
+        return super.lbl_MAIN_PROCESS();
+    }
+
+    @Override
+    protected int lbl_PROCESS_CROSSVAL() {
+        
+        return super.lbl_PROCESS_CROSSVAL();
+    }
+
+    @Override
+    protected int lbl_RAISE_ERROR() {
+        
+        return super.lbl_RAISE_ERROR();
+    }
+
+    @Override
+    protected int lbl_REAL_CROSSVAL() {
+        
+        return super.lbl_REAL_CROSSVAL();
+    }
+
+    @Override
+    protected int lbl_VALIDATE_DUPLICATES() {
+        
+        return super.lbl_VALIDATE_DUPLICATES();
+    }
+
+    @Override
+    protected int lbl_VALIDATE_FIELDS() {
+        
+        return super.lbl_VALIDATE_FIELDS();
+    }
+
+    @Override
+    protected int lbl_VALIDATE_TRIGGER_ACTIVITY() {
+        
+        return super.lbl_VALIDATE_TRIGGER_ACTIVITY();
+    }
+
+    @Override
+    protected int main() {
+        
+        return super.main();
+    }
+
+    @Override
+    public void move() {
+        
+        super.move();
+    }
+
+    @Override
+    public void release() {
+        
+        super.release();
+    }
+
+    @Override
+    public void reset() {
+        
+        super.reset();
+    }
+
+    @Override
+    public String setVarValue(String arg0, String arg1) {
+        
+        return super.setVarValue(arg0, arg1);
+    }
+
+    @Override
+    public void stack(AA_ACTIVITY_PROCESSING_CLASS_VALIDATIONS_cl arg0) {
+        
+        super.stack(arg0);
+    }
+
+    @Override
+    public void stopMoving() {
+        
+        super.stopMoving();
+    }
+
+}
