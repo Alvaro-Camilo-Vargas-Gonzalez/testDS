ff8aeec19f3cbb95dbf2868a8e53f9664d351e9d
Alvaro-Camilo-Vargas-Gonzalez <alvaro.camilo@itssglobal.com>
2025-09-16 12:54:58 -0500

    Add files via upload

---
+++ src/com/AaActivityRestrictionEvaluate.java
@@ -0,0 +1,305 @@
+package com;
+
+import com.temenos.t24.AA_ACTIVITY_RESTRICTION_EVALUATE_cl;
+import com.temenos.t24.component_AA_ActivityCharges_22_cl;
+import com.temenos.t24.component_AA_ActivityRestriction_26_cl;
+import com.temenos.t24.component_AA_Framework_17_cl;
+import com.temenos.t24.component_AA_PricingRules_19_cl;
+import com.temenos.t24.component_AA_ProductBundle_20_cl;
+import com.temenos.t24.component_AA_ProductFramework_23_cl;
+import com.temenos.t24.component_AF_Framework_17_cl;
+import com.temenos.t24.component_EB_SystemTables_19_cl;
+import com.temenos.tafj.common.jSession;
+import com.temenos.tafj.common.jVar;
+
+public class AaActivityRestrictionEvaluate extends AA_ACTIVITY_RESTRICTION_EVALUATE_cl {
+
+    @Override
+    protected void CB(int arg0) {
+       
+        super.CB(arg0);
+    }
+
+    @Override
+    public void CLEAR() {
+        
+        super.CLEAR();
+    }
+
+    @Override
+    protected void GOSUB(int arg0, boolean arg1) {
+        
+        super.GOSUB(arg0, arg1);
+    }
+
+    @Override
+    protected void GOSUB(int arg0) {
+        
+        super.GOSUB(arg0);
+    }
+
+    @Override
+    public void create() {
+        
+        super.create();
+    }
+
+    @Override
+    public component_AA_ActivityCharges_22_cl getAA_ActivityCharges() {
+        
+        return super.getAA_ActivityCharges();
+    }
+
+    @Override
+    public component_AA_ActivityRestriction_26_cl getAA_ActivityRestriction() {
+        
+        return super.getAA_ActivityRestriction();
+    }
+
+    @Override
+    public component_AA_Framework_17_cl getAA_Framework() {
+        
+        return super.getAA_Framework();
+    }
+
+    @Override
+    public component_AA_PricingRules_19_cl getAA_PricingRules() {
+        
+        return super.getAA_PricingRules();
+    }
+
+    @Override
+    public component_AA_ProductBundle_20_cl getAA_ProductBundle() {
+        
+        return super.getAA_ProductBundle();
+    }
+
+    @Override
+    public component_AA_ProductFramework_23_cl getAA_ProductFramework() {
+        
+        return super.getAA_ProductFramework();
+    }
+
+    @Override
+    public component_AF_Framework_17_cl getAF_Framework() {
+        
+        return super.getAF_Framework();
+    }
+
+    @Override
+    public String getBASICName() {
+        
+        return super.getBASICName();
+    }
+
+    @Override
+    public String getCompileInfo() {
+        
+        return super.getCompileInfo();
+    }
+
+    @Override
+    public String[] getComponentList() {
+        
+        return super.getComponentList();
+    }
+
+    @Override
+    public jVar getDataStructureFields(Object arg0) {
+        
+        return super.getDataStructureFields(arg0);
+    }
+
+    @Override
+    public jVar getDataStructureNames() {
+        
+        return super.getDataStructureNames();
+    }
+
+    @Override
+    public component_EB_SystemTables_19_cl getEB_SystemTables() {
+        
+        return super.getEB_SystemTables();
+    }
+
+    @Override
+    public String getImportBasic() {
+        
+        return super.getImportBasic();
+    }
+
+    @Override
+    public int getNbLines() {
+        
+        return super.getNbLines();
+    }
+
+    @Override
+    public String getPackageBasic() {
+        
+        return super.getPackageBasic();
+    }
+
+    @Override
+    public String[] getParamList() {
+        
+        return super.getParamList();
+    }
+
+    @Override
+    public jVar[] getParams() {
+        
+        return super.getParams();
+    }
+
+    @Override
+    public String getPathFileNameBasic() {
+        
+        return super.getPathFileNameBasic();
+    }
+
+    @Override
+    public String getReplacementInfo() {
+        
+        return super.getReplacementInfo();
+    }
+
+    @Override
+    public String[] getVarList() {
+        
+        return super.getVarList();
+    }
+
+    @Override
+    public String getVarValue(String arg0) {
+        
+        return super.getVarValue(arg0);
+    }
+
+    @Override
+    public String getVersion() {
+        
+        return super.getVersion();
+    }
+
+    @Override
+    public void init(jSession arg0) {
+        
+        super.init(arg0);
+    }
+
+    @Override
+    public jVar invoke() {
+        
+        return super.invoke();
+    }
+
+    @Override
+    public jVar invoke(Object... arg0) {
+        
+        return super.invoke(arg0);
+    }
+
+    @Override
+    public void keepMoving() {
+        
+        super.keepMoving();
+    }
+
+    @Override
+    protected int lbl_DELETE_CHARGE_DETAILS() {
+        
+        return super.lbl_DELETE_CHARGE_DETAILS();
+    }
+
+    @Override
+    protected int lbl_Initialise() {
+        
+        return super.lbl_Initialise();
+    }
+
+    @Override
+    protected int lbl_ProcessAction() {
+        
+        return super.lbl_ProcessAction();
+    }
+
+    @Override
+    protected int lbl_ProcessAuthoriseAction() {
+        
+        return super.lbl_ProcessAuthoriseAction();
+    }
+
+    @Override
+    protected int lbl_ProcessDeleteAction() {
+        
+        return super.lbl_ProcessDeleteAction();
+    }
+
+    @Override
+    protected int lbl_ProcessInputAction() {
+        
+        return super.lbl_ProcessInputAction();
+    }
+
+    @Override
+    protected int lbl_ProcessReverseAction() {
+        
+        return super.lbl_ProcessReverseAction();
+    }
+
+    @Override
+    protected int lbl_SetActivityDetails() {
+        
+        return super.lbl_SetActivityDetails();
+    }
+
+    @Override
+    protected int lbl_UPDATE_CHARGE_DETAILS() {
+        
+        return super.lbl_UPDATE_CHARGE_DETAILS();
+    }
+
+    @Override
+    protected int main() {
+        
+        return super.main();
+    }
+
+    @Override
+    public void move() {
+        
+        super.move();
+    }
+
+    @Override
+    public void release() {
+        
+        super.release();
+    }
+
+    @Override
+    public void reset() {
+        
+        super.reset();
+    }
+
+    @Override
+    public String setVarValue(String arg0, String arg1) {
+        
+        return super.setVarValue(arg0, arg1);
+    }
+
+    @Override
+    public void stack(AA_ACTIVITY_RESTRICTION_EVALUATE_cl arg0) {
+        
+        super.stack(arg0);
+    }
+
+    @Override
+    public void stopMoving() {
+        
+        super.stopMoving();
+    }
+
+}
