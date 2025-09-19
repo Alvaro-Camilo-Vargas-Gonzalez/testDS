91dbd9ac52b08f8a486fc9923a0c9dd97003313e
Alvaro-Camilo-Vargas-Gonzalez <alvaro.camilo@itssglobal.com>
2025-09-19 14:15:47 -0500

    Add files via upload

---
+++ src/com/AaActivityRestrictionValidate.java
@@ -0,0 +1,483 @@
+package com;
+
+import com.temenos.t24.AA_ACTIVITY_RESTRICTION_VALIDATE_cl;
+import com.temenos.t24.component_AA_ActivityRestriction_26_cl;
+import com.temenos.t24.component_AA_Fees_12_cl;
+import com.temenos.t24.component_AA_Framework_17_cl;
+import com.temenos.t24.component_AA_Participant_19_cl;
+import com.temenos.t24.component_AA_ProductFramework_23_cl;
+import com.temenos.t24.component_AA_Rules_13_cl;
+import com.temenos.t24.component_AC_SoftAccounting_21_cl;
+import com.temenos.t24.component_AF_Framework_17_cl;
+import com.temenos.t24.component_AF_Rules_13_cl;
+import com.temenos.t24.component_EB_ErrorProcessing_22_cl;
+import com.temenos.t24.component_EB_SystemTables_19_cl;
+import com.temenos.t24.component_EB_Template_16_cl;
+import com.temenos.tafj.common.jSession;
+import com.temenos.tafj.common.jVar;
+
+public class AaActivityRestrictionValidate extends AA_ACTIVITY_RESTRICTION_VALIDATE_cl {
+
+    @Override
+    protected void CB(int arg0) {
+        
+        super.CB(arg0);
+    }
+
+    @Override
+    public void CLEAR() {
+        
+        super.CLEAR();
+    }
+
+    @Override
+    protected void GOSUB(int arg0, boolean arg1) {
+        
+        super.GOSUB(arg0, arg1);
+    }
+
+    @Override
+    protected void GOSUB(int arg0) {
+        
+        super.GOSUB(arg0);
+    }
+
+    @Override
+    public void create() {
+        
+        super.create();
+    }
+
+    @Override
+    public component_AA_ActivityRestriction_26_cl getAA_ActivityRestriction() {
+        
+        return super.getAA_ActivityRestriction();
+    }
+
+    @Override
+    public component_AA_Fees_12_cl getAA_Fees() {
+        
+        return super.getAA_Fees();
+    }
+
+    @Override
+    public component_AA_Framework_17_cl getAA_Framework() {
+        
+        return super.getAA_Framework();
+    }
+
+    @Override
+    public component_AA_Participant_19_cl getAA_Participant() {
+        
+        return super.getAA_Participant();
+    }
+
+    @Override
+    public component_AA_ProductFramework_23_cl getAA_ProductFramework() {
+        
+        return super.getAA_ProductFramework();
+    }
+
+    @Override
+    public component_AA_Rules_13_cl getAA_Rules() {
+        
+        return super.getAA_Rules();
+    }
+
+    @Override
+    public component_AC_SoftAccounting_21_cl getAC_SoftAccounting() {
+        
+        return super.getAC_SoftAccounting();
+    }
+
+    @Override
+    public component_AF_Framework_17_cl getAF_Framework() {
+        
+        return super.getAF_Framework();
+    }
+
+    @Override
+    public component_AF_Rules_13_cl getAF_Rules() {
+        
+        return super.getAF_Rules();
+    }
+
+    @Override
+    public String getBASICName() {
+        
+        return super.getBASICName();
+    }
+
+    @Override
+    public String getCompileInfo() {
+        
+        return super.getCompileInfo();
+    }
+
+    @Override
+    public String[] getComponentList() {
+        
+        return super.getComponentList();
+    }
+
+    @Override
+    public jVar getDataStructureFields(Object arg0) {
+        
+        return super.getDataStructureFields(arg0);
+    }
+
+    @Override
+    public jVar getDataStructureNames() {
+        
+        return super.getDataStructureNames();
+    }
+
+    @Override
+    public component_EB_ErrorProcessing_22_cl getEB_ErrorProcessing() {
+        
+        return super.getEB_ErrorProcessing();
+    }
+
+    @Override
+    public component_EB_SystemTables_19_cl getEB_SystemTables() {
+        
+        return super.getEB_SystemTables();
+    }
+
+    @Override
+    public component_EB_Template_16_cl getEB_Template() {
+        
+        return super.getEB_Template();
+    }
+
+    @Override
+    public String getImportBasic() {
+        
+        return super.getImportBasic();
+    }
+
+    @Override
+    public int getNbLines() {
+        
+        return super.getNbLines();
+    }
+
+    @Override
+    public String getPackageBasic() {
+        
+        return super.getPackageBasic();
+    }
+
+    @Override
+    public String[] getParamList() {
+        
+        return super.getParamList();
+    }
+
+    @Override
+    public jVar[] getParams() {
+        
+        return super.getParams();
+    }
+
+    @Override
+    public String getPathFileNameBasic() {
+        
+        return super.getPathFileNameBasic();
+    }
+
+    @Override
+    public String getReplacementInfo() {
+        
+        return super.getReplacementInfo();
+    }
+
+    @Override
+    public String[] getVarList() {
+        
+        return super.getVarList();
+    }
+
+    @Override
+    public String getVarValue(String arg0) {
+        
+        return super.getVarValue(arg0);
+    }
+
+    @Override
+    public String getVersion() {
+        
+        return super.getVersion();
+    }
+
+    @Override
+    public void init(jSession arg0) {
+        
+        super.init(arg0);
+    }
+
+    @Override
+    public jVar invoke() {
+        
+        return super.invoke();
+    }
+
+    @Override
+    public jVar invoke(Object... arg0) {
+        
+        return super.invoke(arg0);
+    }
+
+    @Override
+    public void keepMoving() {
+        
+        super.keepMoving();
+    }
+
+    @Override
+    protected int lbl_ARRANGEMENT_CROSSVAL() {
+        
+        return super.lbl_ARRANGEMENT_CROSSVAL();
+    }
+
+    @Override
+    protected int lbl_ARRANGEMENT_DEFAULTS() {
+        
+        return super.lbl_ARRANGEMENT_DEFAULTS();
+    }
+
+    @Override
+    protected int lbl_AUTH_CROSSVAL() {
+        
+        return super.lbl_AUTH_CROSSVAL();
+    }
+
+    @Override
+    protected int lbl_CHECK_PR_ATTR_PROPERTY_CLASS() {
+        
+        return super.lbl_CHECK_PR_ATTR_PROPERTY_CLASS();
+    }
+
+    @Override
+    protected int lbl_COMMON_CROSSVAL() {
+        
+        return super.lbl_COMMON_CROSSVAL();
+    }
+
+    @Override
+    protected int lbl_DEFAULT_ACTIVITY_ID() {
+        
+        return super.lbl_DEFAULT_ACTIVITY_ID();
+    }
+
+    @Override
+    protected int lbl_DEFAULT_RULE_NAME() {
+        
+        return super.lbl_DEFAULT_RULE_NAME();
+    }
+
+    @Override
+    protected int lbl_DELETE_CROSSVAL() {
+        
+        return super.lbl_DELETE_CROSSVAL();
+    }
+
+    @Override
+    protected int lbl_DESIGNER_CROSSVAL() {
+        
+        return super.lbl_DESIGNER_CROSSVAL();
+    }
+
+    @Override
+    protected int lbl_DESIGNER_DEFAULTS() {
+        
+        return super.lbl_DESIGNER_DEFAULTS();
+    }
+
+    @Override
+    protected int lbl_DUP_AND_NULL_CHECK() {
+        
+        return super.lbl_DUP_AND_NULL_CHECK();
+    }
+
+    @Override
+    protected int lbl_GETPERIODICRULECATALOG() {
+        
+        return super.lbl_GETPERIODICRULECATALOG();
+    }
+
+    @Override
+    protected int lbl_GET_FROM_CHARGE_PROPERTY() {
+        
+        return super.lbl_GET_FROM_CHARGE_PROPERTY();
+    }
+
+    @Override
+    protected int lbl_GET_PARTICIPANT_DETAILS() {
+        
+        return super.lbl_GET_PARTICIPANT_DETAILS();
+    }
+
+    @Override
+    protected int lbl_GET_PROPERTY_RECORD() {
+        
+        return super.lbl_GET_PROPERTY_RECORD();
+    }
+
+    @Override
+    protected int lbl_HANDLE_ERR() {
+        
+        return super.lbl_HANDLE_ERR();
+    }
+
+    @Override
+    protected int lbl_HANDLE_ERROR() {
+        
+        return super.lbl_HANDLE_ERROR();
+    }
+
+    @Override
+    protected int lbl_INITIALISE() {
+        
+        return super.lbl_INITIALISE();
+    }
+
+    @Override
+    protected int lbl_PROCESS_CROSSVAL() {
+        
+        return super.lbl_PROCESS_CROSSVAL();
+    }
+
+    @Override
+    protected int lbl_READ_PERIODIC_ATTRIBUTE() {
+        
+        return super.lbl_READ_PERIODIC_ATTRIBUTE();
+    }
+
+    @Override
+    protected int lbl_READ_PERIODIC_ATTRIBUTE_CLASS() {
+        
+        return super.lbl_READ_PERIODIC_ATTRIBUTE_CLASS();
+    }
+
+    @Override
+    protected int lbl_REAL_CROSSVAL() {
+        
+        return super.lbl_REAL_CROSSVAL();
+    }
+
+    @Override
+    protected int lbl_REVERSE_CROSSVAL() {
+        
+        return super.lbl_REVERSE_CROSSVAL();
+    }
+
+    @Override
+    protected int lbl_VALIDATE_ACTIVITY_FIELDS() {
+        
+        return super.lbl_VALIDATE_ACTIVITY_FIELDS();
+    }
+
+    @Override
+    protected int lbl_VALIDATE_CHARGE_PROPERTY() {
+        
+        return super.lbl_VALIDATE_CHARGE_PROPERTY();
+    }
+
+    @Override
+    protected int lbl_VALIDATE_PARTICIPANT_CHARGE() {
+        
+        return super.lbl_VALIDATE_PARTICIPANT_CHARGE();
+    }
+
+    @Override
+    protected int lbl_VALIDATE_PROPERTIES() {
+        
+        return super.lbl_VALIDATE_PROPERTIES();
+    }
+
+    @Override
+    protected int lbl_VALIDATE_PROPERTY_FIELDS() {
+        
+        return super.lbl_VALIDATE_PROPERTY_FIELDS();
+    }
+
+    @Override
+    protected int lbl_VALIDATE_RULE_FIELDS() {
+        
+        return super.lbl_VALIDATE_RULE_FIELDS();
+    }
+
+    @Override
+    protected int lbl_VALIDATE_SYSTEM_BALANCE_TYPE() {
+        
+        return super.lbl_VALIDATE_SYSTEM_BALANCE_TYPE();
+    }
+
+    @Override
+    protected int lbl_VALIDATE_WAIVE_CHARGE_FOR_PARTICIPANT() {
+        
+        return super.lbl_VALIDATE_WAIVE_CHARGE_FOR_PARTICIPANT();
+    }
+
+    @Override
+    protected int lbl_ValidBalanceType() {
+        
+        return super.lbl_ValidBalanceType();
+    }
+
+    @Override
+    protected int lbl_ValidateBalanceNetting() {
+        
+        return super.lbl_ValidateBalanceNetting();
+    }
+
+    @Override
+    protected int lbl_ValidateRuleSet() {
+        
+        return super.lbl_ValidateRuleSet();
+    }
+
+    @Override
+    protected int main() {
+        
+        return super.main();
+    }
+
+    @Override
+    public void move() {
+        
+        super.move();
+    }
+
+    @Override
+    public void release() {
+        
+        super.release();
+    }
+
+    @Override
+    public void reset() {
+        
+        super.reset();
+    }
+
+    @Override
+    public String setVarValue(String arg0, String arg1) {
+        
+        return super.setVarValue(arg0, arg1);
+    }
+
+    @Override
+    public void stack(AA_ACTIVITY_RESTRICTION_VALIDATE_cl arg0) {
+        
+        super.stack(arg0);
+    }
+
+    @Override
+    public void stopMoving() {
+        
+        super.stopMoving();
+    }
+
+}
