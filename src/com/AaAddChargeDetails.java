83e90f7dea6129c8dd612f39680cfb3e19ee11d5
Alvaro-Camilo-Vargas-Gonzalez <alvaro.camilo@itssglobal.com>
2025-09-23 11:17:32 -0500

    Add files via upload

---
+++ src/com/AaAddChargeDetails.java
@@ -0,0 +1,426 @@
+package com;
+
+import com.temenos.t24.AA_ADD_CHARGE_DETAILS_cl;
+import com.temenos.t24.component_AA_ActivityCharges_22_cl;
+import com.temenos.t24.component_AA_ActivityRestriction_26_cl;
+import com.temenos.t24.component_AA_ChargeOff_16_cl;
+import com.temenos.t24.component_AA_ChargeOverride_21_cl;
+import com.temenos.t24.component_AA_Fees_12_cl;
+import com.temenos.t24.component_AA_Framework_17_cl;
+import com.temenos.t24.component_AA_Interest_16_cl;
+import com.temenos.t24.component_AA_PeriodicCharges_22_cl;
+import com.temenos.t24.component_AA_PricingRules_19_cl;
+import com.temenos.t24.component_AA_ProductBundle_20_cl;
+import com.temenos.t24.component_AA_ProductFramework_23_cl;
+import com.temenos.t24.component_AA_ProductManagement_24_cl;
+import com.temenos.t24.component_AA_Settlement_18_cl;
+import com.temenos.t24.component_AA_Tax_11_cl;
+import com.temenos.t24.component_AA_TermAmount_17_cl;
+import com.temenos.t24.component_AF_Framework_17_cl;
+import com.temenos.t24.component_EB_Interface_17_cl;
+import com.temenos.t24.component_EB_OverrideProcessing_25_cl;
+import com.temenos.t24.component_EB_SystemTables_19_cl;
+import com.temenos.t24.component_ST_CompanyCreation_22_cl;
+import com.temenos.tafj.common.jSession;
+import com.temenos.tafj.common.jVar;
+
+public class AaAddChargeDetails extends AA_ADD_CHARGE_DETAILS_cl {
+
+    @Override
+    protected void CB(int arg0) {
+        
+        super.CB(arg0);
+    }
+
+    @Override
+    public void CLEAR() {
+        
+        super.CLEAR();
+    }
+
+    @Override
+    protected void GOSUB(int arg0, boolean arg1) {
+        
+        super.GOSUB(arg0, arg1);
+    }
+
+    @Override
+    protected void GOSUB(int arg0) {
+        
+        super.GOSUB(arg0);
+    }
+
+    @Override
+    public void create() {
+        
+        super.create();
+    }
+
+    @Override
+    public component_AA_ActivityCharges_22_cl getAA_ActivityCharges() {
+        
+        return super.getAA_ActivityCharges();
+    }
+
+    @Override
+    public component_AA_ActivityRestriction_26_cl getAA_ActivityRestriction() {
+        
+        return super.getAA_ActivityRestriction();
+    }
+
+    @Override
+    public component_AA_ChargeOff_16_cl getAA_ChargeOff() {
+        
+        return super.getAA_ChargeOff();
+    }
+
+    @Override
+    public component_AA_ChargeOverride_21_cl getAA_ChargeOverride() {
+        
+        return super.getAA_ChargeOverride();
+    }
+
+    @Override
+    public component_AA_Fees_12_cl getAA_Fees() {
+        
+        return super.getAA_Fees();
+    }
+
+    @Override
+    public component_AA_Framework_17_cl getAA_Framework() {
+        
+        return super.getAA_Framework();
+    }
+
+    @Override
+    public component_AA_Interest_16_cl getAA_Interest() {
+        
+        return super.getAA_Interest();
+    }
+
+    @Override
+    public component_AA_PeriodicCharges_22_cl getAA_PeriodicCharges() {
+        
+        return super.getAA_PeriodicCharges();
+    }
+
+    @Override
+    public component_AA_PricingRules_19_cl getAA_PricingRules() {
+        
+        return super.getAA_PricingRules();
+    }
+
+    @Override
+    public component_AA_ProductBundle_20_cl getAA_ProductBundle() {
+        
+        return super.getAA_ProductBundle();
+    }
+
+    @Override
+    public component_AA_ProductFramework_23_cl getAA_ProductFramework() {
+        
+        return super.getAA_ProductFramework();
+    }
+
+    @Override
+    public component_AA_ProductManagement_24_cl getAA_ProductManagement() {
+        
+        return super.getAA_ProductManagement();
+    }
+
+    @Override
+    public component_AA_Settlement_18_cl getAA_Settlement() {
+        
+        return super.getAA_Settlement();
+    }
+
+    @Override
+    public component_AA_Tax_11_cl getAA_Tax() {
+        
+        return super.getAA_Tax();
+    }
+
+    @Override
+    public component_AA_TermAmount_17_cl getAA_TermAmount() {
+        
+        return super.getAA_TermAmount();
+    }
+
+    @Override
+    public component_AF_Framework_17_cl getAF_Framework() {
+        
+        return super.getAF_Framework();
+    }
+
+    @Override
+    public String getBASICName() {
+        
+        return super.getBASICName();
+    }
+
+    @Override
+    public String getCompileInfo() {
+        
+        return super.getCompileInfo();
+    }
+
+    @Override
+    public String[] getComponentList() {
+        
+        return super.getComponentList();
+    }
+
+    @Override
+    public jVar getDataStructureFields(Object arg0) {
+        
+        return super.getDataStructureFields(arg0);
+    }
+
+    @Override
+    public jVar getDataStructureNames() {
+        
+        return super.getDataStructureNames();
+    }
+
+    @Override
+    public component_EB_Interface_17_cl getEB_Interface() {
+        
+        return super.getEB_Interface();
+    }
+
+    @Override
+    public component_EB_OverrideProcessing_25_cl getEB_OverrideProcessing() {
+        
+        return super.getEB_OverrideProcessing();
+    }
+
+    @Override
+    public component_EB_SystemTables_19_cl getEB_SystemTables() {
+        
+        return super.getEB_SystemTables();
+    }
+
+    @Override
+    public String getImportBasic() {
+        
+        return super.getImportBasic();
+    }
+
+    @Override
+    public int getNbLines() {
+        
+        return super.getNbLines();
+    }
+
+    @Override
+    public String getPackageBasic() {
+        
+        return super.getPackageBasic();
+    }
+
+    @Override
+    public String[] getParamList() {
+        
+        return super.getParamList();
+    }
+
+    @Override
+    public jVar[] getParams() {
+        
+        return super.getParams();
+    }
+
+    @Override
+    public String getPathFileNameBasic() {
+        
+        return super.getPathFileNameBasic();
+    }
+
+    @Override
+    public String getReplacementInfo() {
+        
+        return super.getReplacementInfo();
+    }
+
+    @Override
+    public component_ST_CompanyCreation_22_cl getST_CompanyCreation() {
+        
+        return super.getST_CompanyCreation();
+    }
+
+    @Override
+    public String[] getVarList() {
+        
+        return super.getVarList();
+    }
+
+    @Override
+    public String getVarValue(String arg0) {
+        
+        return super.getVarValue(arg0);
+    }
+
+    @Override
+    public String getVersion() {
+        
+        return super.getVersion();
+    }
+
+    @Override
+    public void init(jSession arg0) {
+        
+        super.init(arg0);
+    }
+
+    @Override
+    public jVar invoke(Object arg0, Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6,
+            Object arg7, Object arg8, Object arg9, Object arg10, Object arg11) {
+        
+        return super.invoke(arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11);
+    }
+
+    @Override
+    public jVar invoke(Object... arg0) {
+        
+        return super.invoke(arg0);
+    }
+
+    @Override
+    public void keepMoving() {
+        
+        super.keepMoving();
+    }
+
+    @Override
+    protected int lbl_BUILD_ISSUEBILL_ACTIVITY_ID() {
+        
+        return super.lbl_BUILD_ISSUEBILL_ACTIVITY_ID();
+    }
+
+    @Override
+    protected int lbl_BUILD_NEW_ACTIVITY_RECORD() {
+        
+        return super.lbl_BUILD_NEW_ACTIVITY_RECORD();
+    }
+
+    @Override
+    protected int lbl_CALC_CHARGE_DUE_DATE() {
+        
+        return super.lbl_CALC_CHARGE_DUE_DATE();
+    }
+
+    @Override
+    protected int lbl_CHECK_CHARGEOFF_STATUS() {
+        
+        return super.lbl_CHECK_CHARGEOFF_STATUS();
+    }
+
+    @Override
+    protected int lbl_CHECK_REQD_INFO() {
+        
+        return super.lbl_CHECK_REQD_INFO();
+    }
+
+    @Override
+    protected int lbl_GET_CHARGE_RECORD() {
+        
+        return super.lbl_GET_CHARGE_RECORD();
+    }
+
+    @Override
+    protected int lbl_INITIALISE() {
+        
+        return super.lbl_INITIALISE();
+    }
+
+    @Override
+    protected int lbl_MAIN_PROCESS() {
+        
+        return super.lbl_MAIN_PROCESS();
+    }
+
+    @Override
+    protected int lbl_PROCESS_ACTIVITY_CHARGES() {
+        
+        return super.lbl_PROCESS_ACTIVITY_CHARGES();
+    }
+
+    @Override
+    protected int lbl_PROCESS_ACTIVITY_OR_UPDATE_HANDOFF() {
+        
+        return super.lbl_PROCESS_ACTIVITY_OR_UPDATE_HANDOFF();
+    }
+
+    @Override
+    protected int lbl_PROCESS_NEW_ACTIVITY() {
+        
+        return super.lbl_PROCESS_NEW_ACTIVITY();
+    }
+
+    @Override
+    protected int lbl_UPDATE_HANDOFF_DETAILS() {
+        
+        return super.lbl_UPDATE_HANDOFF_DETAILS();
+    }
+
+    @Override
+    protected int lbl_UPDATE_PAYMENT_DATE() {
+        
+        return super.lbl_UPDATE_PAYMENT_DATE();
+    }
+
+    @Override
+    protected int lbl_UPDATE_PRICING_DETAILS() {
+        
+        return super.lbl_UPDATE_PRICING_DETAILS();
+    }
+
+    @Override
+    protected int lbl_UPDATE_TAX_AMOUNT() {
+        
+        return super.lbl_UPDATE_TAX_AMOUNT();
+    }
+
+    @Override
+    protected int main() {
+        
+        return super.main();
+    }
+
+    @Override
+    public void move() {
+        
+        super.move();
+    }
+
+    @Override
+    public void release() {
+        
+        super.release();
+    }
+
+    @Override
+    public void reset() {
+        
+        super.reset();
+    }
+
+    @Override
+    public String setVarValue(String arg0, String arg1) {
+        
+        return super.setVarValue(arg0, arg1);
+    }
+
+    @Override
+    public void stack(AA_ADD_CHARGE_DETAILS_cl arg0) {
+        
+        super.stack(arg0);
+    }
+
+    @Override
+    public void stopMoving() {
+        
+        super.stopMoving();
+    }
+
+}
