e1176f42d1e1c36ea02eff3cbc19132ea2d7ee8f
Alvaro-Camilo-Vargas-Gonzalez <alvaro.camilo@itssglobal.com>
2025-09-23 12:13:50 -0500

    Add files via upload

---
+++ src/com/AaAddReplayActivity.java
@@ -0,0 +1,447 @@
+package com;
+
+import com.temenos.t24.AA_ADD_REPLAY_ACTIVITY_cl;
+import com.temenos.t24.component_AA_Account_15_cl;
+import com.temenos.t24.component_AA_Framework_17_cl;
+import com.temenos.t24.component_AA_Interest_16_cl;
+import com.temenos.t24.component_AA_PaymentSchedule_22_cl;
+import com.temenos.t24.component_AA_ProductFramework_23_cl;
+import com.temenos.t24.component_AF_Framework_17_cl;
+import com.temenos.t24.component_EB_Interface_17_cl;
+import com.temenos.t24.component_EB_Service_15_cl;
+import com.temenos.t24.component_EB_SystemTables_19_cl;
+import com.temenos.t24.component_EB_TransactionControl_25_cl;
+import com.temenos.t24.component_EB_Utility_15_cl;
+import com.temenos.t24.component_ST_CompanyCreation_22_cl;
+import com.temenos.tafj.common.jSession;
+import com.temenos.tafj.common.jVar;
+
+public class AaAddReplayActivity extends AA_ADD_REPLAY_ACTIVITY_cl {
+
+    @Override
+    protected void CB(int arg0) {
+        
+        super.CB(arg0);
+    }
+
+    @Override
+    public void CLEAR() {
+        
+        super.CLEAR();
+    }
+
+    @Override
+    protected void GOSUB(int arg0, boolean arg1) {
+        
+        super.GOSUB(arg0, arg1);
+    }
+
+    @Override
+    protected void GOSUB(int arg0) {
+        
+        super.GOSUB(arg0);
+    }
+
+    @Override
+    public void create() {
+        
+        super.create();
+    }
+
+    @Override
+    protected jVar fOPTION_SELECTED(Object arg0, Object arg1) {
+        
+        return super.fOPTION_SELECTED(arg0, arg1);
+    }
+
+    @Override
+    public component_AA_Account_15_cl getAA_Account() {
+        
+        return super.getAA_Account();
+    }
+
+    @Override
+    public component_AA_Framework_17_cl getAA_Framework() {
+        
+        return super.getAA_Framework();
+    }
+
+    @Override
+    public component_AA_Interest_16_cl getAA_Interest() {
+        
+        return super.getAA_Interest();
+    }
+
+    @Override
+    public component_AA_PaymentSchedule_22_cl getAA_PaymentSchedule() {
+        
+        return super.getAA_PaymentSchedule();
+    }
+
+    @Override
+    public component_AA_ProductFramework_23_cl getAA_ProductFramework() {
+        
+        return super.getAA_ProductFramework();
+    }
+
+    @Override
+    public component_AF_Framework_17_cl getAF_Framework() {
+        
+        return super.getAF_Framework();
+    }
+
+    @Override
+    public String getBASICName() {
+        
+        return super.getBASICName();
+    }
+
+    @Override
+    public String getCompileInfo() {
+        
+        return super.getCompileInfo();
+    }
+
+    @Override
+    public String[] getComponentList() {
+        
+        return super.getComponentList();
+    }
+
+    @Override
+    public jVar getDataStructureFields(Object arg0) {
+        
+        return super.getDataStructureFields(arg0);
+    }
+
+    @Override
+    public jVar getDataStructureNames() {
+        
+        return super.getDataStructureNames();
+    }
+
+    @Override
+    public component_EB_Interface_17_cl getEB_Interface() {
+        
+        return super.getEB_Interface();
+    }
+
+    @Override
+    public component_EB_Service_15_cl getEB_Service() {
+        
+        return super.getEB_Service();
+    }
+
+    @Override
+    public component_EB_SystemTables_19_cl getEB_SystemTables() {
+        
+        return super.getEB_SystemTables();
+    }
+
+    @Override
+    public component_EB_TransactionControl_25_cl getEB_TransactionControl() {
+        
+        return super.getEB_TransactionControl();
+    }
+
+    @Override
+    public component_EB_Utility_15_cl getEB_Utility() {
+        
+        return super.getEB_Utility();
+    }
+
+    @Override
+    public String getImportBasic() {
+        
+        return super.getImportBasic();
+    }
+
+    @Override
+    public int getNbLines() {
+        
+        return super.getNbLines();
+    }
+
+    @Override
+    public String getPackageBasic() {
+        
+        return super.getPackageBasic();
+    }
+
+    @Override
+    public String[] getParamList() {
+        
+        return super.getParamList();
+    }
+
+    @Override
+    public jVar[] getParams() {
+        
+        return super.getParams();
+    }
+
+    @Override
+    public String getPathFileNameBasic() {
+        
+        return super.getPathFileNameBasic();
+    }
+
+    @Override
+    public String getReplacementInfo() {
+        
+        return super.getReplacementInfo();
+    }
+
+    @Override
+    public component_ST_CompanyCreation_22_cl getST_CompanyCreation() {
+        
+        return super.getST_CompanyCreation();
+    }
+
+    @Override
+    public String[] getVarList() {
+        
+        return super.getVarList();
+    }
+
+    @Override
+    public String getVarValue(String arg0) {
+        
+        return super.getVarValue(arg0);
+    }
+
+    @Override
+    public String getVersion() {
+        
+        return super.getVersion();
+    }
+
+    @Override
+    public void init(jSession arg0) {
+        
+        super.init(arg0);
+    }
+
+    @Override
+    public jVar invoke(Object... arg0) {
+        
+        return super.invoke(arg0);
+    }
+
+    @Override
+    public jVar invoke(Object arg0) {
+        
+        return super.invoke(arg0);
+    }
+
+    @Override
+    public void keepMoving() {
+        
+        super.keepMoving();
+    }
+
+    @Override
+    protected int lbl_ACCRUE_TO_DATE() {
+        
+        return super.lbl_ACCRUE_TO_DATE();
+    }
+
+    @Override
+    protected int lbl_ADD_ACTIVITY_LIST() {
+        
+        return super.lbl_ADD_ACTIVITY_LIST();
+    }
+
+    @Override
+    protected int lbl_ADD_ACTIVITY_TO_QUEUE() {
+        
+        return super.lbl_ADD_ACTIVITY_TO_QUEUE();
+    }
+
+    @Override
+    protected int lbl_ADD_LIST_TO_QUEUE() {
+        
+        return super.lbl_ADD_LIST_TO_QUEUE();
+    }
+
+    @Override
+    protected int lbl_ADD_REPLAY_REQUEST() {
+        
+        return super.lbl_ADD_REPLAY_REQUEST();
+    }
+
+    @Override
+    protected int lbl_CHECK_ACTIVITY() {
+        
+        return super.lbl_CHECK_ACTIVITY();
+    }
+
+    @Override
+    protected int lbl_CHECK_ADJUST_BUNDLE_ACTIVITY() {
+        
+        return super.lbl_CHECK_ADJUST_BUNDLE_ACTIVITY();
+    }
+
+    @Override
+    protected int lbl_CHECK_COMPANY() {
+        
+        return super.lbl_CHECK_COMPANY();
+    }
+
+    @Override
+    protected int lbl_CHECK_END_OF_PROCESS() {
+        
+        return super.lbl_CHECK_END_OF_PROCESS();
+    }
+
+    @Override
+    protected int lbl_CHECK_NEXT_ACTIVITY() {
+        
+        return super.lbl_CHECK_NEXT_ACTIVITY();
+    }
+
+    @Override
+    protected int lbl_CHECK_REPLAY_REQUIRED() {
+        
+        return super.lbl_CHECK_REPLAY_REQUIRED();
+    }
+
+    @Override
+    protected int lbl_DELETE_ACTIVITY_LIST() {
+        
+        return super.lbl_DELETE_ACTIVITY_LIST();
+    }
+
+    @Override
+    protected int lbl_DELETE_REPLAY_LIST() {
+        
+        return super.lbl_DELETE_REPLAY_LIST();
+    }
+
+    @Override
+    protected int lbl_DELETE_RR_CONTROL() {
+        
+        return super.lbl_DELETE_RR_CONTROL();
+    }
+
+    @Override
+    protected int lbl_DEL_REV_REAL_MASTER_PENDING() {
+        
+        return super.lbl_DEL_REV_REAL_MASTER_PENDING();
+    }
+
+    @Override
+    protected int lbl_FIND_MASTER_ID() {
+        
+        return super.lbl_FIND_MASTER_ID();
+    }
+
+    @Override
+    protected int lbl_GET_NEXT_ACTIVITY() {
+        
+        return super.lbl_GET_NEXT_ACTIVITY();
+    }
+
+    @Override
+    protected int lbl_GenerateAAAId() {
+        
+        return super.lbl_GenerateAAAId();
+    }
+
+    @Override
+    protected int lbl_INITIALISE() {
+        
+        return super.lbl_INITIALISE();
+    }
+
+    @Override
+    protected int lbl_LoadPrePostActivity() {
+        
+        return super.lbl_LoadPrePostActivity();
+    }
+
+    @Override
+    protected int lbl_PROCESS() {
+        
+        return super.lbl_PROCESS();
+    }
+
+    @Override
+    protected int lbl_PROCESS_PENDING() {
+        
+        return super.lbl_PROCESS_PENDING();
+    }
+
+    @Override
+    protected int lbl_PROCESS_PENDING_ACTIVITY() {
+        
+        return super.lbl_PROCESS_PENDING_ACTIVITY();
+    }
+
+    @Override
+    protected int lbl_SEARCH_FOR_NEXT_ACTIVITY() {
+        
+        return super.lbl_SEARCH_FOR_NEXT_ACTIVITY();
+    }
+
+    @Override
+    protected int lbl_SET_REAL_MASTER_COMPLETE() {
+        
+        return super.lbl_SET_REAL_MASTER_COMPLETE();
+    }
+
+    @Override
+    protected int lbl_UPDATE_CONTROL() {
+        
+        return super.lbl_UPDATE_CONTROL();
+    }
+
+    @Override
+    protected int lbl_WRITE_TO_RRCONTROL() {
+        
+        return super.lbl_WRITE_TO_RRCONTROL();
+    }
+
+    @Override
+    protected int main() {
+        
+        return super.main();
+    }
+
+    @Override
+    public void move() {
+        
+        super.move();
+    }
+
+    @Override
+    public void release() {
+        
+        super.release();
+    }
+
+    @Override
+    public void reset() {
+        
+        super.reset();
+    }
+
+    @Override
+    public String setVarValue(String arg0, String arg1) {
+        
+        return super.setVarValue(arg0, arg1);
+    }
+
+    @Override
+    public void stack(AA_ADD_REPLAY_ACTIVITY_cl arg0) {
+        
+        super.stack(arg0);
+    }
+
+    @Override
+    public void stopMoving() {
+        
+        super.stopMoving();
+    }
+
+}
