f1947a7c0571919dbb935ed516894d0243269efc
Alvaro-Camilo-Vargas-Gonzalez <alvaro.camilo@itssglobal.com>
2025-10-22 12:36:28 -0500

    Add files via upload

---
+++ src/com/AaAllocateChargeoffAmount.java
@@ -0,0 +1,297 @@
+package com;
+
+import com.temenos.t24.AA_ALLOCATE_CHARGEOFF_AMOUNT_cl;
+import com.temenos.t24.component_AA_ChargeOff_16_cl;
+import com.temenos.t24.component_AA_Framework_17_cl;
+import com.temenos.t24.component_AA_ProductFramework_23_cl;
+import com.temenos.t24.component_AF_Framework_17_cl;
+import com.temenos.tafj.common.jSession;
+import com.temenos.tafj.common.jVar;
+
+public class AaAllocateChargeoffAmount extends AA_ALLOCATE_CHARGEOFF_AMOUNT_cl {
+
+    @Override
+    protected void CB(int arg0) {
+        
+        super.CB(arg0);
+    }
+
+    @Override
+    public void CLEAR() {
+        
+        super.CLEAR();
+    }
+
+    @Override
+    protected void GOSUB(int arg0, boolean arg1) {
+        
+        super.GOSUB(arg0, arg1);
+    }
+
+    @Override
+    protected void GOSUB(int arg0) {
+        
+        super.GOSUB(arg0);
+    }
+
+    @Override
+    public void create() {
+        
+        super.create();
+    }
+
+    @Override
+    public component_AA_ChargeOff_16_cl getAA_ChargeOff() {
+        
+        return super.getAA_ChargeOff();
+    }
+
+    @Override
+    public component_AA_Framework_17_cl getAA_Framework() {
+        
+        return super.getAA_Framework();
+    }
+
+    @Override
+    public component_AA_ProductFramework_23_cl getAA_ProductFramework() {
+        
+        return super.getAA_ProductFramework();
+    }
+
+    @Override
+    public component_AF_Framework_17_cl getAF_Framework() {
+        
+        return super.getAF_Framework();
+    }
+
+    @Override
+    public String getBASICName() {
+        
+        return super.getBASICName();
+    }
+
+    @Override
+    public String getCompileInfo() {
+        
+        return super.getCompileInfo();
+    }
+
+    @Override
+    public String[] getComponentList() {
+        
+        return super.getComponentList();
+    }
+
+    @Override
+    public jVar getDataStructureFields(Object arg0) {
+        
+        return super.getDataStructureFields(arg0);
+    }
+
+    @Override
+    public jVar getDataStructureNames() {
+        
+        return super.getDataStructureNames();
+    }
+
+    @Override
+    public String getImportBasic() {
+        
+        return super.getImportBasic();
+    }
+
+    @Override
+    public int getNbLines() {
+        
+        return super.getNbLines();
+    }
+
+    @Override
+    public String getPackageBasic() {
+        
+        return super.getPackageBasic();
+    }
+
+    @Override
+    public String[] getParamList() {
+        
+        return super.getParamList();
+    }
+
+    @Override
+    public jVar[] getParams() {
+        
+        return super.getParams();
+    }
+
+    @Override
+    public String getPathFileNameBasic() {
+        
+        return super.getPathFileNameBasic();
+    }
+
+    @Override
+    public String getReplacementInfo() {
+        
+        return super.getReplacementInfo();
+    }
+
+    @Override
+    public String[] getVarList() {
+        
+        return super.getVarList();
+    }
+
+    @Override
+    public String getVarValue(String arg0) {
+        
+        return super.getVarValue(arg0);
+    }
+
+    @Override
+    public String getVersion() {
+        
+        return super.getVersion();
+    }
+
+    @Override
+    public void init(jSession arg0) {
+        
+        super.init(arg0);
+    }
+
+    @Override
+    public jVar invoke(Object arg0, Object arg1, Object arg2, Object arg3, Object arg4, Object arg5, Object arg6,
+            Object arg7, Object arg8, Object arg9, Object arg10, Object arg11, Object arg12, Object arg13,
+            Object arg14) {
+        
+        return super.invoke(arg0, arg1, arg2, arg3, arg4, arg5, arg6, arg7, arg8, arg9, arg10, arg11, arg12, arg13, arg14);
+    }
+
+    @Override
+    public jVar invoke(Object... arg0) {
+        
+        return super.invoke(arg0);
+    }
+
+    @Override
+    public void keepMoving() {
+        
+        super.keepMoving();
+    }
+
+    @Override
+    protected int lbl_ALLOCATE_BILL_CHARGEOFF_AMOUNT() {
+        
+        return super.lbl_ALLOCATE_BILL_CHARGEOFF_AMOUNT();
+    }
+
+    @Override
+    protected int lbl_ALLOCATE_CURRENT_CHARGEOFF_AMOUNT() {
+        
+        return super.lbl_ALLOCATE_CURRENT_CHARGEOFF_AMOUNT();
+    }
+
+    @Override
+    protected int lbl_ALLOCATE_PROPERTY_AMOUNT() {
+        
+        return super.lbl_ALLOCATE_PROPERTY_AMOUNT();
+    }
+
+    @Override
+    protected int lbl_APPLY_CHARGEOFF_RULES() {
+        
+        return super.lbl_APPLY_CHARGEOFF_RULES();
+    }
+
+    @Override
+    protected int lbl_EXTRACT_REQD_INFO() {
+        
+        return super.lbl_EXTRACT_REQD_INFO();
+    }
+
+    @Override
+    protected int lbl_GET_ACCOUNT_PROPERTY() {
+        
+        return super.lbl_GET_ACCOUNT_PROPERTY();
+    }
+
+    @Override
+    protected int lbl_GET_PROPERTY_AMOUNT() {
+        
+        return super.lbl_GET_PROPERTY_AMOUNT();
+    }
+
+    @Override
+    protected int lbl_INITIALISE() {
+        
+        return super.lbl_INITIALISE();
+    }
+
+    @Override
+    protected int lbl_INSERT_CHARGEOFF_DETAILS() {
+        
+        return super.lbl_INSERT_CHARGEOFF_DETAILS();
+    }
+
+    @Override
+    protected int lbl_PROCESS_CHARGEOFF_AMOUNT() {
+        
+        return super.lbl_PROCESS_CHARGEOFF_AMOUNT();
+    }
+
+    @Override
+    protected int lbl_UPDATE_CHARGEOFF_AMOUNT() {
+        
+        return super.lbl_UPDATE_CHARGEOFF_AMOUNT();
+    }
+
+    @Override
+    protected int lbl_UPDATE_REMAINDER_AMOUNT() {
+        
+        return super.lbl_UPDATE_REMAINDER_AMOUNT();
+    }
+
+    @Override
+    protected int main() {
+        
+        return super.main();
+    }
+
+    @Override
+    public void move() {
+        
+        super.move();
+    }
+
+    @Override
+    public void release() {
+        
+        super.release();
+    }
+
+    @Override
+    public void reset() {
+        
+        super.reset();
+    }
+
+    @Override
+    public String setVarValue(String arg0, String arg1) {
+        
+        return super.setVarValue(arg0, arg1);
+    }
+
+    @Override
+    public void stack(AA_ALLOCATE_CHARGEOFF_AMOUNT_cl arg0) {
+        
+        super.stack(arg0);
+    }
+
+    @Override
+    public void stopMoving() {
+        
+        super.stopMoving();
+    }
+
+}
