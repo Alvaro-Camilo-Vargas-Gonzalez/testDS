37b9d1312a7c63cb4847bb239fb49a6c62ba9dfc
Alvaro-Camilo-Vargas-Gonzalez <alvaro.camilo@itssglobal.com>
2025-10-28 14:41:59 -0500

    Add files via upload

---
+++ src/com/AaAltPaymentMethodWrapper.java
@@ -0,0 +1,13 @@
+package com;
+
+import com.temenos.t24.AA_ALT_PAYMENT_METHOD_WRAPPER_cl;
+
+/**
+ * TODO: Document me!
+ *
+ * @author alcas
+ *
+ */
+public class AaAltPaymentMethodWrapper extends AA_ALT_PAYMENT_METHOD_WRAPPER_cl {
+
+}
