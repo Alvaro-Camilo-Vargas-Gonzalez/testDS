dd1eb4db9b806a36cacc76635af1283cac54589f
Alvaro-Camilo-Vargas-Gonzalez <alvaro.camilo@itssglobal.com>
2025-11-05 10:27:59 -0500

    Add files via upload

---
+++ src/com/AaArchiveActivityHistory.java
@@ -0,0 +1,276 @@
+package com;
+
+import com.temenos.t24.AA_ARCHIVE_ACTIVITY_HISTORY_cl;
+import com.temenos.t24.component_AA_Framework_17_cl;
+import com.temenos.t24.component_AA_ProductFramework_23_cl;
+import com.temenos.t24.component_EB_SystemTables_19_cl;
+import com.temenos.tafj.common.jSession;
+import com.temenos.tafj.common.jVar;
+
+public class AaArchiveActivityHistory extends AA_ARCHIVE_ACTIVITY_HISTORY_cl {
+
+    @Override
+    protected void CB(int arg0) {
+        
+        super.CB(arg0);
+    }
+
+    @Override
+    public void CLEAR() {
+        
+        super.CLEAR();
+    }
+
+    @Override
+    protected void GOSUB(int arg0, boolean arg1) {
+        
+        super.GOSUB(arg0, arg1);
+    }
+
+    @Override
+    protected void GOSUB(int arg0) {
+        
+        super.GOSUB(arg0);
+    }
+
+    @Override
+    public void create() {
+        
+        super.create();
+    }
+
+    @Override
+    public component_AA_Framework_17_cl getAA_Framework() {
+        
+        return super.getAA_Framework();
+    }
+
+    @Override
+    public component_AA_ProductFramework_23_cl getAA_ProductFramework() {
+        
+        return super.getAA_ProductFramework();
+    }
+
+    @Override
+    public String getBASICName() {
+        
+        return super.getBASICName();
+    }
+
+    @Override
+    public String getCompileInfo() {
+        
+        return super.getCompileInfo();
+    }
+
+    @Override
+    public String[] getComponentList() {
+        
+        return super.getComponentList();
+    }
+
+    @Override
+    public jVar getDataStructureFields(Object arg0) {
+        
+        return super.getDataStructureFields(arg0);
+    }
+
+    @Override
+    public jVar getDataStructureNames() {
+        
+        return super.getDataStructureNames();
+    }
+
+    @Override
+    public component_EB_SystemTables_19_cl getEB_SystemTables() {
+        
+        return super.getEB_SystemTables();
+    }
+
+    @Override
+    public String getImportBasic() {
+        
+        return super.getImportBasic();
+    }
+
+    @Override
+    public int getNbLines() {
+        
+        return super.getNbLines();
+    }
+
+    @Override
+    public String getPackageBasic() {
+        
+        return super.getPackageBasic();
+    }
+
+    @Override
+    public String[] getParamList() {
+        
+        return super.getParamList();
+    }
+
+    @Override
+    public jVar[] getParams() {
+        
+        return super.getParams();
+    }
+
+    @Override
+    public String getPathFileNameBasic() {
+        
+        return super.getPathFileNameBasic();
+    }
+
+    @Override
+    public String getReplacementInfo() {
+        
+        return super.getReplacementInfo();
+    }
+
+    @Override
+    public String[] getVarList() {
+        
+        return super.getVarList();
+    }
+
+    @Override
+    public String getVarValue(String arg0) {
+        
+        return super.getVarValue(arg0);
+    }
+
+    @Override
+    public String getVersion() {
+        
+        return super.getVersion();
+    }
+
+    @Override
+    public void init(jSession arg0) {
+        
+        super.init(arg0);
+    }
+
+    @Override
+    public jVar invoke(Object arg0, Object arg1, Object arg2, Object arg3) {
+        
+        return super.invoke(arg0, arg1, arg2, arg3);
+    }
+
+    @Override
+    public jVar invoke(Object... arg0) {
+        
+        return super.invoke(arg0);
+    }
+
+    @Override
+    public void keepMoving() {
+        
+        super.keepMoving();
+    }
+
+    @Override
+    protected int lbl_ACT_REF_AND_CONTEXT_MERGE() {
+        
+        return super.lbl_ACT_REF_AND_CONTEXT_MERGE();
+    }
+
+    @Override
+    protected int lbl_ARCHIVE_ACTIVITY_HISTORY() {
+        
+        return super.lbl_ARCHIVE_ACTIVITY_HISTORY();
+    }
+
+    @Override
+    protected int lbl_ARCHIVE_CONTEXT_REL_FIELD() {
+        
+        return super.lbl_ARCHIVE_CONTEXT_REL_FIELD();
+    }
+
+    @Override
+    protected int lbl_DO_PROCESS() {
+        
+        return super.lbl_DO_PROCESS();
+    }
+
+    @Override
+    protected int lbl_DO_SAME_DAY_PROCESS() {
+        
+        return super.lbl_DO_SAME_DAY_PROCESS();
+    }
+
+    @Override
+    protected int lbl_GET_EFFECTIVE_DATE_UNDER_ACTREF() {
+        
+        return super.lbl_GET_EFFECTIVE_DATE_UNDER_ACTREF();
+    }
+
+    @Override
+    protected int lbl_INITIALISE() {
+        
+        return super.lbl_INITIALISE();
+    }
+
+    @Override
+    protected int lbl_UPDATE_AA_ACTIVITY_HISTORY_HIST() {
+        
+        return super.lbl_UPDATE_AA_ACTIVITY_HISTORY_HIST();
+    }
+
+    @Override
+    protected int lbl_UPD_DEL_DETAILS() {
+        
+        return super.lbl_UPD_DEL_DETAILS();
+    }
+
+    @Override
+    protected int lbl_UPD_RETAIN_DETAILS() {
+        
+        return super.lbl_UPD_RETAIN_DETAILS();
+    }
+
+    @Override
+    protected int main() {
+        
+        return super.main();
+    }
+
+    @Override
+    public void move() {
+        
+        super.move();
+    }
+
+    @Override
+    public void release() {
+        
+        super.release();
+    }
+
+    @Override
+    public void reset() {
+        
+        super.reset();
+    }
+
+    @Override
+    public String setVarValue(String arg0, String arg1) {
+        
+        return super.setVarValue(arg0, arg1);
+    }
+
+    @Override
+    public void stack(AA_ARCHIVE_ACTIVITY_HISTORY_cl arg0) {
+        
+        super.stack(arg0);
+    }
+
+    @Override
+    public void stopMoving() {
+        
+        super.stopMoving();
+    }
+
+}
