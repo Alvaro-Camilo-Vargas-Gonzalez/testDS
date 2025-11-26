39fff6e6e3d10468336ff2e696c85f81766f0a7b
Alvaro-Camilo-Vargas-Gonzalez <alvaro.camilo@itssglobal.com>
2025-11-26 15:50:15 -0500

    Add files via upload

---
+++ src/com/AaArrProductTracker.java
@@ -0,0 +1,408 @@
+package com;
+
+import com.temenos.t24.AA_ARR_PRODUCT_TRACKER_cl;
+import com.temenos.t24.component_AA_ChangeProduct_20_cl;
+import com.temenos.t24.component_AA_Framework_17_cl;
+import com.temenos.t24.component_AA_ProductFramework_23_cl;
+import com.temenos.t24.component_AA_ProductManagement_24_cl;
+import com.temenos.t24.component_EB_API_9_cl;
+import com.temenos.t24.component_EB_DataAccess_17_cl;
+import com.temenos.t24.component_EB_Service_15_cl;
+import com.temenos.t24.component_EB_SystemTables_19_cl;
+import com.temenos.t24.component_ST_CompanyCreation_22_cl;
+import com.temenos.tafj.common.jSession;
+import com.temenos.tafj.common.jVar;
+
+public class AaArrProductTracker extends AA_ARR_PRODUCT_TRACKER_cl {
+
+    @Override
+    protected void CB(int arg0) {
+        
+        super.CB(arg0);
+    }
+
+    @Override
+    public void CLEAR() {
+        
+        super.CLEAR();
+    }
+
+    @Override
+    protected void GOSUB(int arg0, boolean arg1) {
+        
+        super.GOSUB(arg0, arg1);
+    }
+
+    @Override
+    protected void GOSUB(int arg0) {
+        
+        super.GOSUB(arg0);
+    }
+
+    @Override
+    public void create() {
+        
+        super.create();
+    }
+
+    @Override
+    public component_AA_ChangeProduct_20_cl getAA_ChangeProduct() {
+        
+        return super.getAA_ChangeProduct();
+    }
+
+    @Override
+    public component_AA_Framework_17_cl getAA_Framework() {
+        
+        return super.getAA_Framework();
+    }
+
+    @Override
+    public component_AA_ProductFramework_23_cl getAA_ProductFramework() {
+        
+        return super.getAA_ProductFramework();
+    }
+
+    @Override
+    public component_AA_ProductManagement_24_cl getAA_ProductManagement() {
+        
+        return super.getAA_ProductManagement();
+    }
+
+    @Override
+    public String getBASICName() {
+        
+        return super.getBASICName();
+    }
+
+    @Override
+    public String getCompileInfo() {
+        
+        return super.getCompileInfo();
+    }
+
+    @Override
+    public String[] getComponentList() {
+        
+        return super.getComponentList();
+    }
+
+    @Override
+    public jVar getDataStructureFields(Object arg0) {
+        
+        return super.getDataStructureFields(arg0);
+    }
+
+    @Override
+    public jVar getDataStructureNames() {
+        
+        return super.getDataStructureNames();
+    }
+
+    @Override
+    public component_EB_API_9_cl getEB_API() {
+        
+        return super.getEB_API();
+    }
+
+    @Override
+    public component_EB_DataAccess_17_cl getEB_DataAccess() {
+        
+        return super.getEB_DataAccess();
+    }
+
+    @Override
+    public component_EB_Service_15_cl getEB_Service() {
+        
+        return super.getEB_Service();
+    }
+
+    @Override
+    public component_EB_SystemTables_19_cl getEB_SystemTables() {
+        
+        return super.getEB_SystemTables();
+    }
+
+    @Override
+    public String getImportBasic() {
+        
+        return super.getImportBasic();
+    }
+
+    @Override
+    public int getNbLines() {
+        
+        return super.getNbLines();
+    }
+
+    @Override
+    public String getPackageBasic() {
+        
+        return super.getPackageBasic();
+    }
+
+    @Override
+    public String[] getParamList() {
+        
+        return super.getParamList();
+    }
+
+    @Override
+    public jVar[] getParams() {
+        
+        return super.getParams();
+    }
+
+    @Override
+    public String getPathFileNameBasic() {
+        
+        return super.getPathFileNameBasic();
+    }
+
+    @Override
+    public String getReplacementInfo() {
+        
+        return super.getReplacementInfo();
+    }
+
+    @Override
+    public component_ST_CompanyCreation_22_cl getST_CompanyCreation() {
+        
+        return super.getST_CompanyCreation();
+    }
+
+    @Override
+    public String[] getVarList() {
+        
+        return super.getVarList();
+    }
+
+    @Override
+    public String getVarValue(String arg0) {
+        
+        return super.getVarValue(arg0);
+    }
+
+    @Override
+    public String getVersion() {
+        
+        return super.getVersion();
+    }
+
+    @Override
+    public void init(jSession arg0) {
+        
+        super.init(arg0);
+    }
+
+    @Override
+    public jVar invoke(Object... arg0) {
+        
+        return super.invoke(arg0);
+    }
+
+    @Override
+    public jVar invoke(Object arg0) {
+        
+        return super.invoke(arg0);
+    }
+
+    @Override
+    public void keepMoving() {
+        
+        super.keepMoving();
+    }
+
+    @Override
+    protected int lbl_APPLY_CHANGE() {
+        
+        return super.lbl_APPLY_CHANGE();
+    }
+
+    @Override
+    protected int lbl_CHECK_FAILED_ACTIVITY() {
+        
+        return super.lbl_CHECK_FAILED_ACTIVITY();
+    }
+
+    @Override
+    protected int lbl_CHECK_FOR_PARENT_PRODUCT_RECORD() {
+        
+        return super.lbl_CHECK_FOR_PARENT_PRODUCT_RECORD();
+    }
+
+    @Override
+    protected int lbl_CHECK_INHERITANCE_PROPERTY() {
+        
+        return super.lbl_CHECK_INHERITANCE_PROPERTY();
+    }
+
+    @Override
+    protected int lbl_CHECK_SCHEDULED_DATE() {
+        
+        return super.lbl_CHECK_SCHEDULED_DATE();
+    }
+
+    @Override
+    protected int lbl_CheckForRegionalVarTriggerActivity() {
+        
+        return super.lbl_CheckForRegionalVarTriggerActivity();
+    }
+
+    @Override
+    protected int lbl_CheckPropertyOrPropClass() {
+        
+        return super.lbl_CheckPropertyOrPropClass();
+    }
+
+    @Override
+    protected int lbl_DETERMINE_ARR_PRODUCT() {
+        
+        return super.lbl_DETERMINE_ARR_PRODUCT();
+    }
+
+    @Override
+    protected int lbl_DETERMINE_CONDITION() {
+        
+        return super.lbl_DETERMINE_CONDITION();
+    }
+
+    @Override
+    protected int lbl_DETERMINE_PROCESS_REQUIRED() {
+        
+        return super.lbl_DETERMINE_PROCESS_REQUIRED();
+    }
+
+    @Override
+    protected int lbl_DO_PROCESS() {
+        
+        return super.lbl_DO_PROCESS();
+    }
+
+    @Override
+    protected int lbl_DetermineRegVariationEligibility() {
+        
+        return super.lbl_DetermineRegVariationEligibility();
+    }
+
+    @Override
+    protected int lbl_GENERATE_NEW_AAA() {
+        
+        return super.lbl_GENERATE_NEW_AAA();
+    }
+
+    @Override
+    protected int lbl_GET_AAA_ID() {
+        
+        return super.lbl_GET_AAA_ID();
+    }
+
+    @Override
+    protected int lbl_GET_PROPERTY_ACTIVITY() {
+        
+        return super.lbl_GET_PROPERTY_ACTIVITY();
+    }
+
+    @Override
+    protected int lbl_GET_PROPERTY_UPGRADE() {
+        
+        return super.lbl_GET_PROPERTY_UPGRADE();
+    }
+
+    @Override
+    protected int lbl_INITIALISE() {
+        
+        return super.lbl_INITIALISE();
+    }
+
+    @Override
+    protected int lbl_LOAD_FAILED_ACTIVITY_DETAILS() {
+        
+        return super.lbl_LOAD_FAILED_ACTIVITY_DETAILS();
+    }
+
+    @Override
+    protected int lbl_PROCESS_CHANGES() {
+        
+        return super.lbl_PROCESS_CHANGES();
+    }
+
+    @Override
+    protected int lbl_PROCESS_CONDITION() {
+        
+        return super.lbl_PROCESS_CONDITION();
+    }
+
+    @Override
+    protected int lbl_PROCESS_PROPERTY() {
+        
+        return super.lbl_PROCESS_PROPERTY();
+    }
+
+    @Override
+    protected int lbl_SET_FATAL_COMMON() {
+        
+        return super.lbl_SET_FATAL_COMMON();
+    }
+
+    @Override
+    protected int lbl_TriggerOrScheduleActivity() {
+        
+        return super.lbl_TriggerOrScheduleActivity();
+    }
+
+    @Override
+    protected int lbl_UPDATE_PROPERTIES_ACTIVITY() {
+        
+        return super.lbl_UPDATE_PROPERTIES_ACTIVITY();
+    }
+
+    @Override
+    protected int lbl_UPDATE_SCHEDULED_ACTIVITY() {
+        
+        return super.lbl_UPDATE_SCHEDULED_ACTIVITY();
+    }
+
+    @Override
+    protected int main() {
+        
+        return super.main();
+    }
+
+    @Override
+    public void move() {
+        
+        super.move();
+    }
+
+    @Override
+    public void release() {
+        
+        super.release();
+    }
+
+    @Override
+    public void reset() {
+        
+        super.reset();
+    }
+
+    @Override
+    public String setVarValue(String arg0, String arg1) {
+        
+        return super.setVarValue(arg0, arg1);
+    }
+
+    @Override
+    public void stack(AA_ARR_PRODUCT_TRACKER_cl arg0) {
+        
+        super.stack(arg0);
+    }
+
+    @Override
+    public void stopMoving() {
+        
+        super.stopMoving();
+    }
+
+}
